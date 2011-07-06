ff52cfd86b25e7713b687d95501b68c31b7df18a
Mike Hearn <hearn@google.com>
2011-07-06 13:43:46 +0000

    Add a tool that prints out an arbitrary block given its hash.

---
+++ src/com/google/bitcoin/examples/FetchBlock.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.bitcoin.examples;
+
+import com.google.bitcoin.core.*;
+import com.google.bitcoin.store.BlockStore;
+import com.google.bitcoin.store.MemoryBlockStore;
+
+import java.net.InetAddress;
+import java.util.concurrent.Future;
+
+/**
+ * Downloads the block given a block hash from the localhost node and prints it out.
+ */
+public class FetchBlock {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Connecting to node");
+        final NetworkParameters params = NetworkParameters.prodNet();
+        NetworkConnection conn = new NetworkConnection(InetAddress.getLocalHost(), params, 0, 60000);
+        BlockStore blockStore = new MemoryBlockStore(params);
+        BlockChain chain = new BlockChain(params, blockStore);
+        Peer peer = new Peer(params, conn, chain);
+        peer.start();
+
+        Sha256Hash blockHash = new Sha256Hash(args[0]);
+        Future<Block> future = peer.getBlock(blockHash);
+        System.out.println("Waiting for node to send us the requested block: " + blockHash);
+        Block block = future.get();
+        System.out.println(block);
+        peer.disconnect();
+    }
+}
