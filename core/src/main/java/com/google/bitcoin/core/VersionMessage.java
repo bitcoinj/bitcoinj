7705bd59a45f954fecb71e0435047a38c8fa68ae
Mike Hearn <hearn@google.com>
2012-05-21 16:22:26 +0200

    Version 0.6-SNAPSHOT

--- core/src/main/java/com/google/bitcoin/core/VersionMessage.java
+++ core/src/main/java/com/google/bitcoin/core/VersionMessage.java
@@ -92,7 +92,7 @@ public class VersionMessage extends Message {
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);  // Cannot happen (illegal IP length).
         }
-        subVer = "/BitCoinJ:0.5-SNAPSHOT/";
+        subVer = "/BitCoinJ:0.6-SNAPSHOT/";
         bestHeight = newBestHeight;
 
         length = 84;
