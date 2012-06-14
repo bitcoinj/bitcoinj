2d8f25a2da56f96b5dc842d04a3429bbc803b16e
Mike Hearn <hearn@google.com>
2012-06-14 13:42:06 +0200

    Bump locator size to 100 to match coinbase maturity. Add a few more comments to address Mirons review comments for chain download fixes.

--- core/src/main/java/com/google/bitcoin/core/Peer.java
+++ core/src/main/java/com/google/bitcoin/core/Peer.java
@@ -359,7 +359,7 @@ public class Peer {
                 // the others.
                 //
                 // We must do two things here:
-                // (1) Request from current top of chain to the root of the current set of orphan blocks and
+                // (1) Request from current top of chain to the oldest ancestor of the received block in the orphan set
                 // (2) Filter out duplicate getblock requests (done in blockChainDownload).
                 //
                 // The reason for (1) is that otherwise if new blocks were solved during the middle of chain download
@@ -595,7 +595,8 @@ public class Peer {
     }
 
     // Keep track of the last request we made to the peer in blockChainDownload so we can avoid redundant and harmful
-    // getblocks requests.
+    // getblocks requests. This does not have to be synchronized because blockChainDownload cannot be called from
+    // multiple threads simultaneously.
     private Sha256Hash lastGetBlocksBegin, lastGetBlocksEnd;
 
     private void blockChainDownload(Sha256Hash toHash) throws IOException {
@@ -654,7 +655,7 @@ public class Peer {
         log.info("{}: blockChainDownload({}) current head = {}", new Object[] { toString(),
                 toHash.toString(), chainHead.getHeader().getHashAsString() });
         StoredBlock cursor = chainHead;
-        for (int i = 50; cursor != null && i > 0; i--) {
+        for (int i = 100; cursor != null && i > 0; i--) {
             blockLocator.add(cursor.getHeader().getHash());
             try {
                 cursor = cursor.getPrev(store);
