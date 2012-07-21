d20c185253ce869200e8e5d3272c3cb232748041
Mike Hearn <hearn@google.com>
2012-07-21 17:29:18 +0200

    Re-order DNS seeds to match upstream. Pieters node comes first as it's the best maintained, Jeffs last as it's just a static list.

--- core/src/main/java/com/google/bitcoin/discovery/DnsDiscovery.java
+++ core/src/main/java/com/google/bitcoin/discovery/DnsDiscovery.java
@@ -41,10 +41,10 @@ public class DnsDiscovery implements PeerDiscovery {
     private NetworkParameters netParams;
 
     public static final String[] defaultHosts = new String[]{
-            "dnsseed.bluematt.me",         // Matt Corallo
-            "bitseed.xf2.org",             // Jeff Garzik
             "seed.bitcoin.sipa.be",        // Pieter Wuille
+            "dnsseed.bluematt.me",         // Matt Corallo
             "dnsseed.bitcoin.dashjr.org",  // Luke Dashjr
+            "bitseed.xf2.org",             // Jeff Garzik
     };
 
     /**
