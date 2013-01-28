43e1d084cb66bdf5d199876ecfcf23c760245cd5
Mike Hearn <hearn@google.com>
2013-01-28 16:20:43 +0100

    Add Matts copyright notice to TestWithPeerGroup

--- core/src/test/java/com/google/bitcoin/core/TestWithPeerGroup.java
+++ core/src/test/java/com/google/bitcoin/core/TestWithPeerGroup.java
@@ -1,20 +1,28 @@
+/*
+ * Copyright 2012 Matt Corallo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.bitcoin.core;
 
-import static org.junit.Assert.assertTrue;
+import com.google.bitcoin.store.BlockStore;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.*;
 
 import java.net.InetSocketAddress;
 
-import org.jboss.netty.bootstrap.ClientBootstrap;
-import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelFactory;
-import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.channel.ChannelSink;
-import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.Channels;
-import org.junit.Before;
-
-import com.google.bitcoin.store.BlockStore;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Utility class that makes it easy to work with mock NetworkConnections in PeerGroups.
