586f961b1b6b180016b62858a8de1f871d2e7c60
Mike Hearn <hearn@google.com>
2013-02-04 14:47:21 +0100

    Minor WalletTool fixes: 1) Print balance in friendly form 2) Use PeerGroup.stopAndWait() to avoid trying to close the store in parallel with network threads shutdown.

--- tools/src/main/java/com/google/bitcoin/tools/WalletTool.java
+++ tools/src/main/java/com/google/bitcoin/tools/WalletTool.java
@@ -416,7 +416,7 @@ public class WalletTool {
             Wallet.SendRequest req = Wallet.SendRequest.forTx(t);
             req.fee = fee;
             if (!wallet.completeTx(req)) {
-                System.err.println("Insufficient funds: have " + wallet.getBalance());
+                System.err.println("Insufficient funds: have " + Utils.bitcoinValueToFriendlyString(wallet.getBalance()));
                 return;
             }
             try {
@@ -593,7 +593,7 @@ public class WalletTool {
     private static void shutdown() {
         try {
             if (peers == null) return;  // setup() never called so nothing to do.
-            peers.stop();
+            peers.stopAndWait();
             saveWallet(walletFile);
             store.close();
             wallet = null;
