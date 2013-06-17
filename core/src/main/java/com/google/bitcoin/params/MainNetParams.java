bfbc9c0cf6944cc34d9d57881f8bc70fb4046b90
Mike Hearn <hearn@google.com>
2013-06-17 16:11:20 +0200

    Don't use my DNS seed. It's not running on secure hosting so I shut it down.

--- core/src/main/java/com/google/bitcoin/params/MainNetParams.java
+++ core/src/main/java/com/google/bitcoin/params/MainNetParams.java
@@ -60,7 +60,6 @@ public class MainNetParams extends NetworkParameters {
                 "seed.bitcoin.sipa.be",        // Pieter Wuille
                 "dnsseed.bluematt.me",         // Matt Corallo
                 "dnsseed.bitcoin.dashjr.org",  // Luke Dashjr
-                "dnsseed.plan99.net",          // Mike Hearn
         };
     }
 
