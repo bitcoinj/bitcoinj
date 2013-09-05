bf228cce34ec291b3e2218f8ca362428e0b8136d
Mike Hearn <hearn@google.com>
2013-09-05 15:10:50 +0200

    Wallet: comment for AllowUnconfirmedCoinSelector.get()

--- core/src/main/java/com/google/bitcoin/core/Wallet.java
+++ core/src/main/java/com/google/bitcoin/core/Wallet.java
@@ -244,7 +244,7 @@ public class Wallet implements Serializable, BlockChainListener, PeerFilterProvi
 
     /**
      * This coin selector will select any transaction at all, regardless of where it came from or whether it was
-     * confirmed yet.
+     * confirmed yet. However immature coinbases will not be included (would be a protocol violation).
      */
     public static class AllowUnconfirmedCoinSelector extends DefaultCoinSelector {
         @Override protected boolean shouldSelect(Transaction tx) {
@@ -252,6 +252,8 @@ public class Wallet implements Serializable, BlockChainListener, PeerFilterProvi
         }
 
         private static AllowUnconfirmedCoinSelector instance;
+
+        /** Returns a global static instance of the selector. */
         public static AllowUnconfirmedCoinSelector get() {
             // This doesn't have to be thread safe as the object has no state, so discarded duplicates are harmless.
             if (instance == null)
