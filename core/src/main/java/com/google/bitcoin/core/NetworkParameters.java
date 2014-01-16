8ba89238cafb1982877744e37c03cca294827c77
Derf Shaya <derfshaya@gmail.com>
2014-01-17 04:48:04 +0800

    Zetacoin: commented away checkpoints, removed seedAddrs and filled out scriptSig for Genesis block input

--- core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
+++ core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
@@ -109,7 +109,7 @@ public abstract class NetworkParameters implements Serializable {
             //
             //   "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
             byte[] bytes = Hex.decode
-                    ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+                    ("04ffff001d01044533204175672032303133202d204d2647202d204d75676162652077696e73205a696d20656c656374696f6e2077697468206d6f7265207468616e20363025206f6620766f746573");
             t.addInput(new TransactionInput(n, t, bytes));
             ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
             Script.writeBytes(scriptPubKeyBytes, Hex.decode
