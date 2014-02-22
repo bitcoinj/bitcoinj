9e1af0f838c6a8c4c9abae23113164c376154d04
Sean Gilligan <sean@msgilligan.com>
2014-02-21 23:32:54 -0800

    SeedPeers reads from smaller addr files for 3 nets
     * Text files for production, test, and unit test
     * Files have IP addresses only
     * 600, 25, and 1 address each respectively
     * Improved implementation and unit tests

--- core/src/main/java/com/google/bitcoin/net/discovery/SeedPeers.java
+++ core/src/main/java/com/google/bitcoin/net/discovery/SeedPeers.java
@@ -18,12 +18,8 @@ package com.google.bitcoin.net.discovery;
 import com.google.bitcoin.core.NetworkParameters;
 import com.google.common.base.Charsets;
 
-import javax.annotation.Nullable;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
+import java.io.*;
+import java.net.Inet6Address;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -32,88 +28,107 @@ import java.util.concurrent.TimeUnit;
 /**
  * SeedPeers contains a pre-determined list of Bitcoin node addresses. These nodes are selected based on being
  * active on the network for a long period of time. The intention is to be a last resort way of finding a connection
- * to the network, in case IRC and DNS fail. The list is stored in the resource file named <code>resourceFileName</code>.
+ * to the network, in case IRC and DNS fail.
+ * <p>
+ * In the current implementation, the list is stored in a file named <code>resourceFileName</code>.
+ * This file will be stored in a resource in the same package as this class, except on Android which doesn't allow this.
+ * <p>
+ * The file is a text file containing string-formatted ipv4 and/or ipv6 addresses, one-per-line.
+ * ipv6 addresses are currently ignored, but if the <code>ipv4only</code> variable is set to <code>false</code>, both
+ * types will be used. There is no API for setting <code>ipv4only</code> to <code>false</code>,
+ * we'd have to add code to determine that ipv6 addresses
+ * could be used.
  */
 public class SeedPeers implements PeerDiscovery {
     private NetworkParameters params;
     private InetSocketAddress[] allPeers = null;
     private BufferedReader peerFileReader;
-    private static final String resourceFileName = "seeds.txt";
-    private static final int expectedNumberSeeds = 198552;  // Number of expected seeds in "seeds.txt"
+    private boolean ipv4only = true;
+    private static final String resourceFileSuffix = "-seeds.txt";
+    private static final int expectedNumberSeeds = 600;  // Number of expected seeds in stream (initial array size)
+    private static final String alternateFilePath = "assets/";
 
-    public SeedPeers(NetworkParameters params) {
-        this.params = params;
-        InputStream is = this.getClass().getResourceAsStream(resourceFileName);
-        InputStreamReader isr = new InputStreamReader(is, Charsets.UTF_8);
+    /**
+     * Private constructor that could be made public if we want to allow using an arbitrary InputStream
+     * as a source for Peer IP addresses.
+     * @param seedInput
+     * @param params
+     */
+    private SeedPeers(InputStream seedInput, NetworkParameters params) {
+        InputStreamReader isr = new InputStreamReader(seedInput, Charsets.UTF_8);
         this.peerFileReader = new BufferedReader(isr);
-        try {
-            this.peerFileReader.readLine(); // Read and discard header line
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.params = params;
     }
 
     /**
-     * Acts as an iterator, returning the address of each node in the list sequentially.
-     * Once all the list has been iterated, null will be returned for each subsequent query.
-     *
-     * @return InetSocketAddress - The address/port of the next node.
-     * @deprecated - As far as I know this was only used in the unit test and can be removed.
-     * @throws PeerDiscoveryException
+     * Construct a list of SeedPeers from a file configured in NetworkParameters.
+     * Uses <code>hasResources</code> to determine whether to use resource or file path to create InputStream
+     * @param params
+     * @throws FileNotFoundException
      */
-    @Nullable
-    protected InetSocketAddress getPeer() throws PeerDiscoveryException {
-        try {
-            return nextPeer();
-        } catch (UnknownHostException e) {
-            throw new PeerDiscoveryException(e);
-        }
+    public SeedPeers(NetworkParameters params) throws FileNotFoundException {
+        this(hasResources() ?
+                SeedPeers.class.getResourceAsStream(params.getId() + resourceFileSuffix) :
+                new FileInputStream(alternateFilePath + params.getId() + resourceFileSuffix),
+                params);
     }
 
     /**
-     * When getPeer is removed, this should probably be merged inside the loop in allPeers and eliminated.
-     * @return
-     * @deprecated
-     * @throws UnknownHostException
+     * Does the current platform support resources in a JAR file.
+     * @return Should return true most everywhere but Android.
      */
-    @Nullable
-    private InetSocketAddress nextPeer() throws UnknownHostException {
-        try {
-            String line = peerFileReader.readLine();
-            if (line != null) {
-                String address = line.substring(0,line.indexOf(':'));   // Get IPv4 or IPv6 address, but not port
-                return new InetSocketAddress(address, params.getPort());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    static boolean hasResources() {
+        // TODO: return false if this is android (or maybe use feature testing if alternateFilePath unchanged)
+        return true;      // Need a way to determine if this is Android and we can't use JAR resources
     }
 
     /**
      * Returns an array containing all the Bitcoin nodes within the list.
      * This is the only method in the <code>PeerDiscovery</code> interface that returns peer data.
+     * @param timeoutValue ignored, we're assuming file is small enough that time to read is approx zero
+     * @param timeoutUnit also ignored
+     * @return array containing IP addresses of all the peers in the seed address file
+     * @throws PeerDiscoveryException
      */
     @Override
     public InetSocketAddress[] getPeers(long timeoutValue, TimeUnit timeoutUnit) throws PeerDiscoveryException {
         try {
-            return allPeers();
+            if (allPeers == null) {
+                allPeers = loadAllPeers();
+            }
+            return allPeers;
         } catch (UnknownHostException e) {
             throw new PeerDiscoveryException(e);
         }
     }
 
-    private InetSocketAddress[] allPeers() throws UnknownHostException, PeerDiscoveryException {
-        if (allPeers == null) {
-            ArrayList<InetSocketAddress> addressList = new ArrayList<InetSocketAddress>(expectedNumberSeeds);
-            InetSocketAddress peer;
-            while ((peer = nextPeer()) != null) {
-                addressList.add(peer);
+    /**
+     * Return an array of InetSocketAddress containing Peer addresses
+     * @return
+     * @throws UnknownHostException
+     * @throws PeerDiscoveryException
+     */
+    private InetSocketAddress[] loadAllPeers() throws UnknownHostException, PeerDiscoveryException {
+        ArrayList<InetSocketAddress> addressList = new ArrayList<InetSocketAddress>(expectedNumberSeeds);
+
+        try {
+            String line;
+            while ((line = peerFileReader.readLine()) != null) {
+                InetSocketAddress sockAddr;
+                // Use port from params
+                sockAddr = new InetSocketAddress(line, params.getPort());
+                if (!(ipv4only && sockAddr.getAddress() instanceof Inet6Address)) {
+                    // Add address to list (unless ipv4only mode and we have an ipv6 address)
+                    addressList.add(sockAddr);
+                }
             }
-            InetSocketAddress[] addresses = new InetSocketAddress[addressList.size()];
-            allPeers = addressList.toArray(addresses);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new PeerDiscoveryException(e);
         }
-        return allPeers;
+
+        InetSocketAddress[] addresses = new InetSocketAddress[addressList.size()];
+        return addressList.toArray(addresses);
     }
 
     public void shutdown() {
--- core/src/test/java/com/google/bitcoin/net/discovery/SeedPeersTest.java
+++ core/src/test/java/com/google/bitcoin/net/discovery/SeedPeersTest.java
@@ -15,10 +15,17 @@
  */
 package com.google.bitcoin.net.discovery;
 
+import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.params.MainNetParams;
+import com.google.bitcoin.params.TestNet3Params;
+import com.google.bitcoin.params.UnitTestParams;
+import com.google.common.net.InetAddresses;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -26,41 +33,45 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class SeedPeersTest {
-    private static final int numRecordsInResourceFile = 198552;  // Number of lines (minus header) in "seeds.txt"
+    private static HashMap<String, Integer> numEntries = null;  // Number of entries in address file
+    private static ArrayList<NetworkParameters> networkList = null;
 
-    /**
-     * Test single invocation of public (but not in the PeerDiscovery interface) getPeer() method.
-     * @throws Exception
-     */
+    @BeforeClass
+    public static void setUpClass() {
+        SeedPeersTest.numEntries = new HashMap<String, Integer>(3);
+        SeedPeersTest.numEntries.put(NetworkParameters.ID_MAINNET, 600);    // 600 entries in this file
+        SeedPeersTest.numEntries.put(NetworkParameters.ID_TESTNET, 25);     // 25 in this one
+        SeedPeersTest.numEntries.put(NetworkParameters.ID_UNITTESTNET, 1);  // 1 entry (127.0.0.1, is that right?)
+        SeedPeersTest.networkList = new ArrayList<NetworkParameters>(3);
+        SeedPeersTest.networkList.add(MainNetParams.get());
+        SeedPeersTest.networkList.add(TestNet3Params.get());
+        SeedPeersTest.networkList.add(UnitTestParams.get());
+    }
+
     @Test
-    public void getPeer_one() throws Exception{
-        SeedPeers seedPeers = new SeedPeers(MainNetParams.get());
-        assertThat(seedPeers.getPeer(), notNullValue());
+    public void checkHasResource() {
+        // TODO: Once implemented properly test needs to verify based upon platform
+        assertThat(SeedPeers.hasResources(), equalTo(true));
     }
 
     /**
-     * Call getPeer() once for every line in the resource file with no errors,
-     * then call it one more time and make sure we get a null.
+     * Test getPeers() - the only method defined in the PeerDiscovery interface that returns peers.
+     * Test it for ID_MAINNET, ID_TESTNET, and ID_UNITTESTNET
+     * Make sure it returns the expected number of addresses for each case (test needs to be updated if numbers change)
      * @throws Exception
      */
     @Test
-    public void getPeer_all() throws Exception{
-        SeedPeers seedPeers = new SeedPeers(MainNetParams.get());
-        for(int i = 0; i < numRecordsInResourceFile; ++i){
-            assertThat("Failed on index: "+i, seedPeers.getPeer(), notNullValue());
+    public void getPeersCheckArray() throws Exception {
+        // Test for all networks configured in SeedPeersTest.networkList
+        for (NetworkParameters params : SeedPeersTest.networkList) {
+            SeedPeers seedPeers = new SeedPeers(params);
+            InetSocketAddress[] addresses = seedPeers.getPeers(0, TimeUnit.SECONDS);
+            assertThat(addresses, notNullValue());
+            assertThat(addresses.length, equalTo( SeedPeersTest.numEntries.get(params.getId()) ));
+            for (InetSocketAddress address : addresses) {
+                assertThat("Port should match params", address.getPort(), equalTo(params.getPort()));
+                assertThat("Should be IP address not hostname", InetAddresses.isInetAddress(address.getHostString()), equalTo(true));
+            }
         }
-        assertThat(seedPeers.getPeer(), equalTo(null));
-    }
-
-    /**
-     * Call getPeers() - the only method defined in the PeerDiscovery interface that returns peers.
-     * Make sure it returns the expected number of addresses.
-     * @throws Exception
-     */
-    @Test
-    public void getPeers_length() throws Exception{
-        SeedPeers seedPeers = new SeedPeers(MainNetParams.get());
-        InetSocketAddress[] addresses = seedPeers.getPeers(0, TimeUnit.SECONDS);
-        assertThat(addresses.length, equalTo(numRecordsInResourceFile));
     }
 }
