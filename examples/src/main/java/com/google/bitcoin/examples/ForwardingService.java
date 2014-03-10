16aeea910462902eafc704d59dd2dfd9a67cdb1c
Mike Hearn <mike@plan99.net>
2014-03-10 17:36:32 +0100

    Correct maven instruction in the README file and make ForwardingService work on mainnet again by fixing command line arg parsing.
    Resolves issue 523.

--- examples/src/main/java/com/google/bitcoin/examples/ForwardingService.java
+++ examples/src/main/java/com/google/bitcoin/examples/ForwardingService.java
@@ -43,7 +43,7 @@ public class ForwardingService {
     public static void main(String[] args) throws Exception {
         // This line makes the log output more compact and easily read, especially when using the JDK log adapter.
         BriefLogFormatter.init();
-        if (args.length < 2) {
+        if (args.length < 1) {
             System.err.println("Usage: address-to-send-back-to [regtest|testnet]");
             return;
         }
@@ -51,10 +51,10 @@ public class ForwardingService {
         // Figure out which network we should connect to. Each one gets its own set of files.
         NetworkParameters params;
         String filePrefix;
-        if (args[1].equals("testnet")) {
+        if (args.length > 1 && args[1].equals("testnet")) {
             params = TestNet3Params.get();
             filePrefix = "forwarding-service-testnet";
-        } else if (args[1].equals("regtest")) {
+        } else if (args.length > 1 && args[1].equals("regtest")) {
             params = RegTestParams.get();
             filePrefix = "forwarding-service-regtest";
         } else {
