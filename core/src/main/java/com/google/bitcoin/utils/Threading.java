b6149bfa481cbd1f0e7ce6cf10ee2911a7b16fdc
Mike Hearn <mike@plan99.net>
2014-03-12 14:44:26 +0100

    Adjust user thread queue size warning.

--- core/src/main/java/com/google/bitcoin/utils/Threading.java
+++ core/src/main/java/com/google/bitcoin/utils/Threading.java
@@ -86,6 +86,9 @@ public class Threading {
 
     public static class UserThread extends Thread implements Executor {
         private static final Logger log = LoggerFactory.getLogger(UserThread.class);
+        // 10,000 pending tasks is entirely arbitrary and may or may not be appropriate for the device we're
+        // running on.
+        public static int WARNING_THRESHOLD = 10000;
         private LinkedBlockingQueue<Runnable> tasks;
 
         public UserThread() {
@@ -112,10 +115,13 @@ public class Threading {
 
         @Override
         public void execute(Runnable command) {
-            if (tasks.size() > 100) {
-                log.warn("User thread saturated, memory exhaustion may occur.");
-                log.warn("Check for deadlocked or slow event handlers. Sample tasks:");
-                for (Object task : tasks.toArray()) log.warn(task.toString());
+            final int size = tasks.size();
+            if (size > WARNING_THRESHOLD) {
+                log.warn(
+                    "User thread has {} pending tasks, memory exhaustion may occur.\n" +
+                    "If you see this message, check your memory consumption and see if it's problematic or excessively spikey.\n" +
+                    "If it is, check for deadlocked or slow event handlers. If it isn't, try adjusting the constant \n" +
+                    "Threading.UserThread.WARNING_THRESHOLD upwards until it's a suitable level for your app, or Integer.MAX_VALUE to disable." , size);
             }
             Uninterruptibles.putUninterruptibly(tasks, command);
         }
