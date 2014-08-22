512650b4f765c0da3b0136f9e8ff4da6f930db10
Andreas Schildbach <andreas@schildbach.de>
2014-08-23 00:43:50 +0200

    Fix version message. My heart breaks when I see the bad spelling of our dearest child in the logs.

--- core/src/main/java/com/google/bitcoin/core/VersionMessage.java
+++ core/src/main/java/com/google/bitcoin/core/VersionMessage.java
@@ -77,7 +77,7 @@ public class VersionMessage extends Message {
     /** The version of this library release, as a string. */
     public static final String BITCOINJ_VERSION = "0.12-SNAPSHOT";
     /** The value that is prepended to the subVer field of this application. */
-    public static final String LIBRARY_SUBVER = "/BitCoinJ:" + BITCOINJ_VERSION + "/";
+    public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
 
     public VersionMessage(NetworkParameters params, byte[] payload) throws ProtocolException {
         super(params, payload, 0);
