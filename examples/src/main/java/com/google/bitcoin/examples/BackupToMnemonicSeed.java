1cd8050252504485a2c995aed36b905c2b355987
Michael Bumann <michael@railslove.com>
2014-09-16 10:08:38 +0200

    ooops, put the examples to the folder where they belong to

--- examples/src/main/java/com/google/bitcoin/examples/BackupToMnemonicSeed.java
+++ examples/src/main/java/com/google/bitcoin/examples/BackupToMnemonicSeed.java
@@ -1,10 +1,10 @@
-package BackupToMnemonicSeed;
+package com.google.bitcoin.examples;
 
 /**
- * The following example shows you how create a deterministic seed represented as a mnemonic from a wallet. 
- * This seed can be used to restore your wallet. The RestoreFromSeed.java example shows how to restore the wallet from this seed.
+ * The following example shows you how to create a deterministic seed from a hierarchical deterministic wallet represented as a mnemonic code. 
+ * This seed can be used to fully restore your wallet. The RestoreFromSeed.java example shows how to load the wallet from this seed.
  * 
- * In Bitcoin Improvement Proposal (BIP) 39 and BIP 32 describe the details about hierarchical deterministic wallets and mnemonic sentence to represent their seeds 
+ * In Bitcoin Improvement Proposal (BIP) 39 and BIP 32 describe the details about hierarchical deterministic wallets and mnemonic sentences
  * https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
  * https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
  */
