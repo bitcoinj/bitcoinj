4b65e37914f279d35ef1731059546472571e7506
Mike Hearn <mike@plan99.net>
2014-10-07 22:38:09 +0200

    Focus the password field.

--- wallettemplate/src/main/java/wallettemplate/WalletPasswordController.java
+++ wallettemplate/src/main/java/wallettemplate/WalletPasswordController.java
@@ -1,5 +1,6 @@
 package wallettemplate;
 
+import javafx.application.Platform;
 import org.bitcoinj.crypto.KeyCrypterScrypt;
 import com.google.common.primitives.Longs;
 import com.google.protobuf.ByteString;
@@ -43,6 +44,7 @@ public class WalletPasswordController {
 
     public void initialize() {
         progressMeter.setOpacity(0);
+        Platform.runLater(pass1::requestFocus);
     }
 
     @FXML void confirmClicked(ActionEvent event) {
