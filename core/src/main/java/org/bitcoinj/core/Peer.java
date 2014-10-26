b36169caba63c360fe785553f0287952a83d62e2
Adam Mackler <AdamMackler@gmail.com>
2014-10-26 12:28:31 -0400

    Make `TransactionBroadcast` recognize network rejection of transmitted transaction.

--- core/src/main/java/org/bitcoinj/core/Peer.java
+++ core/src/main/java/org/bitcoinj/core/Peer.java
@@ -396,6 +396,8 @@ public class Peer extends PeerSocketHandler {
                 utxosFuture = null;
                 future.set((UTXOsMessage)m);
             }
+        } else if (m instanceof RejectMessage) {
+            log.error("Received Message {}", m);
         } else {
             log.warn("Received unhandled message: {}", m);
         }
