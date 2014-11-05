bitcoinj/bitcoinj

commit e3a13a6efa86432a38ccb402cde2d2d666c11660

When deserializing wallet extensions, lock order is now wallet and then extension.

core/src/main/java/org/bitcoinj/core/Wallet.java
@@ -3972,6 +3972,26 @@ public class Wallet extends BaseTaggableObject implements Serializable, BlockCha
         }
     }
 
+    /** 
+     * Deserialize the wallet extension with the supplied data and add
+     * it to the wallet, unless there exists an extension with the
+     * same id.
+     */ 
+    public void deserializeAndAddExtension(WalletExtension extension, byte[] data) throws Exception {
+        String id = checkNotNull(extension).getWalletExtensionID();
+        lock.lock();
+        try {
+            if (extensions.containsKey(id)) {
+                return;
+            } else {
+                extension.deserializeWalletExtension(this, data);
+                addExtension(extension);
+            }
+        } finally {
+            lock.unlock();
+        }        
+    }
+    
     @Override
     public synchronized void setTag(String tag, ByteString value) {
         super.setTag(tag, value);

core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
@@ -513,13 +513,14 @@ public class WalletProtobufSerializer {
             } else {
                 log.info("Loading wallet extension {}", id);
                 try {
-                    extension.deserializeWalletExtension(wallet, extProto.getData().toByteArray());
-                    wallet.addOrGetExistingExtension(extension);
+                    wallet.deserializeAndAddExtension(extension, extProto.getData().toByteArray());
                 } catch (Exception e) {
-                    if (extProto.getMandatory() && requireMandatoryExtensions)
+                    if (extProto.getMandatory() && requireMandatoryExtensions) {
+                        log.error("Error whilst reading extension {}, failing to read wallet", id, e);
                         throw new UnreadableWalletException("Could not parse mandatory extension in wallet: " + id);
-                    else
+                    } else {
                         log.error("Error whilst reading extension {}, ignoring", id, e);
+                    }
                 }
             }
         }

commit 0d5a542f225c38a1b57ac96ebb6f3b7fe606dd7d

Wallet extension: fix a regression introduced by e3a13a6, where in some kinds of app wallet extensions stopped being loaded properly.

core/src/main/java/org/bitcoinj/core/Wallet.java
@@ -4049,26 +4049,21 @@ public class Wallet extends BaseTaggableObject implements Serializable, BlockCha
         }
     }
 
-    /** 
-     * Deserialize the wallet extension with the supplied data and add
-     * it to the wallet, unless there exists an extension with the
-     * same id.
-     */ 
-    public void deserializeAndAddExtension(WalletExtension extension, byte[] data) throws Exception {
-        String id = checkNotNull(extension).getWalletExtensionID();
+    /**
+     * Deserialize the wallet extension with the supplied data and then install it, replacing any existing extension
+     * that may have existed with the same ID.
+     */
+    public void deserializeExtension(WalletExtension extension, byte[] data) throws Exception {
         lock.lock();
         try {
-            if (extensions.containsKey(id)) {
-                return;
-            } else {
-                extension.deserializeWalletExtension(this, data);
-                addExtension(extension);
-            }
+            // This method exists partly to establish a lock ordering of wallet > extension.
+            extension.deserializeWalletExtension(this, data);
+            extensions.put(extension.getWalletExtensionID(), extension);
         } finally {
             lock.unlock();
-        }        
+        }
     }
-    
+
     @Override
     public synchronized void setTag(String tag, ByteString value) {
         super.setTag(tag, value);

core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
@@ -513,7 +513,7 @@ public class WalletProtobufSerializer {
             } else {
                 log.info("Loading wallet extension {}", id);
                 try {
-                    wallet.deserializeAndAddExtension(extension, extProto.getData().toByteArray());
+                    wallet.deserializeExtension(extension, extProto.getData().toByteArray());
                 } catch (Exception e) {
                     if (extProto.getMandatory() && requireMandatoryExtensions) {
                         log.error("Error whilst reading extension {}, failing to read wallet", id, e);