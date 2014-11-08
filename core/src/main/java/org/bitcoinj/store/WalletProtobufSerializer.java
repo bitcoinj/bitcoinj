2769bc32bb840829d6255ec9375d2181db8bad1f
Andreas Schildbach <andreas@schildbach.de>
2014-11-08 10:06:31 +0100

    When reading wallets, catch another possible data inconsistency and throw UnreadableWalletException.

--- core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
+++ core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
@@ -389,6 +389,8 @@ public class WalletProtobufSerializer {
             return readWallet(params, null, walletProto);
         } catch (IOException e) {
             throw new UnreadableWalletException("Could not parse input stream to protobuf", e);
+        } catch (IllegalStateException e) {
+            throw new UnreadableWalletException("Could not parse input stream to protobuf", e);
         }
     }
 
