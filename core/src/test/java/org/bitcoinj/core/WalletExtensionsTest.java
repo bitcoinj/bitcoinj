f410201342758b947534b6db26978f77333f7831
Andreas Schildbach <andreas@schildbach.de>
2014-11-13 01:30:18 +0100

    Fix missing WalletExtensionsTest.tearDown(). This caused a corrupt Wallet.SendRequest.DEFAULT_FEE_PER_KB.

--- core/src/test/java/org/bitcoinj/core/WalletExtensionsTest.java
+++ core/src/test/java/org/bitcoinj/core/WalletExtensionsTest.java
@@ -2,6 +2,7 @@ package org.bitcoinj.core;
 
 import org.bitcoinj.testing.FooWalletExtension;
 import org.bitcoinj.testing.TestWithWallet;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,11 +14,15 @@ public class WalletExtensionsTest extends TestWithWallet {
         super.setUp();
     }
 
+    @After
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
     @Test(expected = java.lang.IllegalStateException.class)
     public void duplicateWalletExtensionTest() {
         wallet.addExtension(new FooWalletExtension("com.whatever.required", true));
         wallet.addExtension(new FooWalletExtension("com.whatever.required", true));
     }
-
-
 }
