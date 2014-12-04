9f83de3b9440b65029f5939930ebeeb76a61d609
Mike Hearn <mike@plan99.net>
2014-12-04 11:28:26 +0100

    Fix lock inversion tags<->wallet lock.

--- core/src/main/java/org/bitcoinj/core/Wallet.java
+++ core/src/main/java/org/bitcoinj/core/Wallet.java
@@ -4019,7 +4019,7 @@ public class Wallet extends BaseTaggableObject implements Serializable, BlockCha
     }
 
     @Override
-    public synchronized void setTag(String tag, ByteString value) {
+    public void setTag(String tag, ByteString value) {
         super.setTag(tag, value);
         saveNow();
     }
