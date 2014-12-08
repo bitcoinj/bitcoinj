b3c761388f7e22d6226522f5ae7d63d2365f46ca
Mike Hearn <mike@plan99.net>
2014-12-08 22:13:40 +0100

    Fix build, somehow this didn't show up before.

--- core/src/test/java/org/bitcoinj/core/TransactionBroadcastTest.java
+++ core/src/test/java/org/bitcoinj/core/TransactionBroadcastTest.java
@@ -72,7 +72,7 @@ public class TransactionBroadcastTest extends TestWithPeerGroup {
     public void fourPeers() throws Exception {
         InboundMessageQueuer[] channels = { connectPeer(1), connectPeer(2), connectPeer(3), connectPeer(4) };
         Transaction tx = new Transaction(params);
-        TransactionBroadcast broadcast = new TransactionBroadcast(peerGroup, tx);
+        TransactionBroadcast broadcast = new TransactionBroadcast(peerGroup, blockChain.getContext(), tx);
         ListenableFuture<Transaction> future = broadcast.broadcast();
         assertFalse(future.isDone());
         // We expect two peers to receive a tx message, and at least one of the others must announce for the future to
