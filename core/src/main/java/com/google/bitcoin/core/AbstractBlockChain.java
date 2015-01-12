ad4f643bf323b331d14587aba42d970469541567
mike belshe <mike@belshe.com>
2015-01-12 12:10:10 -0800

    Don't drop transactions from orphaned blocks.
    Reviewers: ben, wa
    
    Reviewed By: wa
    
    Differential Revision: http://phabricator.bitgo.com/D457

--- core/src/main/java/com/google/bitcoin/core/AbstractBlockChain.java
+++ core/src/main/java/com/google/bitcoin/core/AbstractBlockChain.java
@@ -115,10 +115,15 @@ public abstract class AbstractBlockChain {
             final boolean filtered = filteredTxHashes != null && filteredTxn != null;
             Preconditions.checkArgument((block.transactions == null && filtered)
                                         || (block.transactions != null && !filtered));
-            if (!shouldVerifyTransactions())
-                this.block = block.cloneAsHeader();
-            else
-                this.block = block;
+
+            // BitGo doesn't verify transactions, but if we prune out the actual blocks here,
+            // we can't replay the transactions when the orphan block is later reconnected.
+            this.block = block;
+
+            //if (!shouldVerifyTransactions())
+            //    this.block = block.cloneAsHeader();
+            //else
+            //    this.block = block;
             this.filteredTxHashes = filteredTxHashes;
             this.filteredTxn = filteredTxn;
         }
