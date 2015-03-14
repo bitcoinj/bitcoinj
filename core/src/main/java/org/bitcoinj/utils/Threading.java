8a1b47e6bfdede0784bf205e2230a0175ec14d56
Mike Hearn <mike@plan99.net>
2015-03-13 17:59:24 -0700

    On Android, use non-cycle detecting locks with fairness activated (experimental)

--- core/src/main/java/org/bitcoinj/utils/Threading.java
+++ core/src/main/java/org/bitcoinj/utils/Threading.java
@@ -20,6 +20,7 @@ import com.google.common.util.concurrent.CycleDetectingLockFactory;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.Uninterruptibles;
+import org.bitcoinj.core.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -152,7 +153,10 @@ public class Threading {
     public static CycleDetectingLockFactory factory;
 
     public static ReentrantLock lock(String name) {
-        return factory.newReentrantLock(name);
+        if (Utils.isAndroidRuntime())
+            return new ReentrantLock(true);
+        else
+            return factory.newReentrantLock(name);
     }
 
     public static void warnOnLockCycles() {
