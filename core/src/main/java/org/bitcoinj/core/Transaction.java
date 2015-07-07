16a5e1d64b604930d25490504bcc2a60218816c2
Ross Nicoll <jrn@jrn.me.uk>
2015-07-07 21:05:59 +0100

    Javadoc format corrections

--- core/src/main/java/org/bitcoinj/core/Transaction.java
+++ core/src/main/java/org/bitcoinj/core/Transaction.java
@@ -215,7 +215,7 @@ public class Transaction extends ChildMessage {
     }
 
     /**
-     * Creates a transaction by reading payload starting from offset bytes in. Length of a transaction is fixed.
+     * Creates a transaction by reading payload. Length of a transaction is fixed.
      */
     public Transaction(NetworkParameters params, byte[] payload, @Nullable Message parent, boolean parseLazy, boolean parseRetain, int length)
             throws ProtocolException {
