f18c19f5260a4c38269c50290aa842b9f61e492e
Andreas Schildbach <andreas@schildbach.de>
2015-07-14 20:07:47 +0200

    Release 0.13.1
    Bump of orchid version to 1.1.1, which is necessary because we accidently pushed an incomplete version 1.1 to Maven Central last year.

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -75,7 +75,7 @@ public class VersionMessage extends Message {
     public boolean relayTxesBeforeFilter;
 
     /** The version of this library release, as a string. */
-    public static final String BITCOINJ_VERSION = "0.13";
+    public static final String BITCOINJ_VERSION = "0.13.1";
     /** The value that is prepended to the subVer field of this application. */
     public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
 
