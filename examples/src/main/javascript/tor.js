0f0c7baec9d6e0a874a279c80c599b93e47ca32a
Chris <ctpacia@gmail.com>
2015-07-24 11:36:54 -0400

    Update javascript Tor example to reflect simplified handling of .onion addresses

--- examples/src/main/javascript/tor.js
+++ examples/src/main/javascript/tor.js
@@ -6,19 +6,15 @@ var params = bcj.params.MainNetParams.get();
 var context = new bcj.core.Context(params);
 bcj.utils.BriefLogFormatter.init();
 
-var InetAddress = Java.type("java.net.InetAddress");
+var PeerAddress = Java.type("org.bitcoinj.core.PeerAddress");
 var InetSocketAddress = Java.type("java.net.InetSocketAddress");
 
-// Hack around the fact that PeerAddress assumes nodes have IP addresses. Simple enough for now.
-var OnionAddress = Java.extend(Java.type("org.bitcoinj.core.PeerAddress"), {
-    toSocketAddress: function() {
-        return InetSocketAddress.createUnresolved("hhiv5pnxenvbf4am.onion", params.port);
-    }
-});
+// The PeerAddress class can now handle InetSocketAddresses with hostnames if they are .onion.
+var OnionAddress = InetSocketAddress.createUnresolved("hhiv5pnxenvbf4am.onion", params.port);
 
 var pg = bcj.core.PeerGroup.newWithTor(context, null, new com.subgraph.orchid.TorClient(), false);
-// c'tor is bogus here: the passed in InetAddress will be ignored.
-pg.addAddress(new OnionAddress(InetAddress.localHost, params.port));
+
+pg.addAddress(new PeerAddress(OnionAddress));
 pg.start();
 
 pg.waitForPeers(1).get();
@@ -28,6 +24,5 @@ for each (var peer in pg.connectedPeers) {
     print(peer.peerVersionMessage.subVer);
     peer.ping().get()
 }
-
 
 pg.stop();
