74af6a2c8a26319e60509391859bca958cf6159c
Andreas Schildbach <andreas@schildbach.de>
2015-08-25 17:50:30 +0200

    Set user agent for HTTP discovery.

--- core/src/main/java/org/bitcoinj/net/discovery/HttpDiscovery.java
+++ core/src/main/java/org/bitcoinj/net/discovery/HttpDiscovery.java
@@ -85,8 +85,11 @@ public class HttpDiscovery implements PeerDiscovery {
             HttpUrl.Builder url = HttpUrl.get(details.uri).newBuilder();
             if (services != 0)
                 url.addQueryParameter("srvmask", Long.toString(services));
+            Request.Builder request = new Request.Builder();
+            request.url(url.build());
+            request.addHeader("User-Agent", VersionMessage.LIBRARY_SUBVER); // TODO Add main version.
             log.info("Requesting seeds from {}", url);
-            Response response = client.newCall(new Request.Builder().url(url.build()).build()).execute();
+            Response response = client.newCall(request.build()).execute();
             if (!response.isSuccessful())
                 throw new PeerDiscoveryException("HTTP request failed: " + response.code() + " " + response.message());
             InputStream stream = response.body().byteStream();
