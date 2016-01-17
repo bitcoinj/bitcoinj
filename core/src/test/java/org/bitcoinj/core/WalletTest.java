61cfffd3774176871667b89b5891f8c2c4a2be46
Andreas Schildbach <andreas@schildbach.de>
2016-01-17 18:06:30 +0100

    WIP Extract empty sig constant

--- core/src/test/java/org/bitcoinj/core/WalletTest.java
+++ core/src/test/java/org/bitcoinj/core/WalletTest.java
@@ -3249,6 +3249,8 @@ public class WalletTest extends TestWithWallet {
         assertEquals(200, tx.getInputs().size());
     }
 
+    private static final byte[] EMPTY_SIG = {};
+
     @Test
     public void completeTxPartiallySignedWithDummySigs() throws Exception {
         byte[] dummySig = TransactionSignature.dummy().encodeToBitcoin();
@@ -3257,8 +3259,7 @@ public class WalletTest extends TestWithWallet {
 
     @Test
     public void completeTxPartiallySignedWithEmptySig() throws Exception {
-        byte[] emptySig = {};
-        completeTxPartiallySigned(Wallet.MissingSigsMode.USE_OP_ZERO, emptySig);
+        completeTxPartiallySigned(Wallet.MissingSigsMode.USE_OP_ZERO, EMPTY_SIG);
     }
 
     @Test (expected = ECKey.MissingPrivateKeyException.class)
@@ -3281,14 +3282,12 @@ public class WalletTest extends TestWithWallet {
 
     @Test
     public void completeTxPartiallySignedMarriedWithEmptySig() throws Exception {
-        byte[] emptySig = {};
-        completeTxPartiallySignedMarried(Wallet.MissingSigsMode.USE_OP_ZERO, emptySig);
+        completeTxPartiallySignedMarried(Wallet.MissingSigsMode.USE_OP_ZERO, EMPTY_SIG);
     }
 
     @Test (expected = TransactionSigner.MissingSignatureException.class)
     public void completeTxPartiallySignedMarriedThrows() throws Exception {
-        byte[] emptySig = {};
-        completeTxPartiallySignedMarried(Wallet.MissingSigsMode.THROW, emptySig);
+        completeTxPartiallySignedMarried(Wallet.MissingSigsMode.THROW, EMPTY_SIG);
     }
 
     @Test (expected = TransactionSigner.MissingSignatureException.class)
