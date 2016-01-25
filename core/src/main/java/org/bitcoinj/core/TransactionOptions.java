b05006ee597571d88580cf1050ba04c0e999becc
NicolasDorier <nicolas.dorier@gmail.com>
2016-01-25 22:32:34 +0000

    Can serialize and deserialize transaction with witnesses

---
+++ core/src/main/java/org/bitcoinj/core/TransactionOptions.java
@@ -0,0 +1,7 @@
+package org.bitcoinj.core;
+
+public class TransactionOptions {
+    public static final int NONE = 0;
+    public static final int WITNESS = 1;
+    public static final int ALL = 1;
+}
