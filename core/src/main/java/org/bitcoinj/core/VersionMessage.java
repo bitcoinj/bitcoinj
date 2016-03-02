ff89b0c149c0adafb4fbb50d37ac8373e4e2ce50
Andreas Schildbach <andreas@schildbach.de>
2016-03-02 16:26:08 +0100

    Prepare 0.13.6-SNAPSHOT

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -76,7 +76,7 @@ public class VersionMessage extends Message {
     public boolean relayTxesBeforeFilter;
 
     /** The version of this library release, as a string. */
-    public static final String BITCOINJ_VERSION = "0.13.5";
+    public static final String BITCOINJ_VERSION = "0.13.6-SNAPSHOT";
     /** The value that is prepended to the subVer field of this application. */
     public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
 
