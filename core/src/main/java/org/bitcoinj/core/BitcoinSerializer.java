9b2ad15cd3d7ab48957e5a189277f80ed5271e8c
Willem Noort <willem@coblue.eu>
2016-03-24 13:52:19 +0100

    Fix Integer overflow in BitcoinSerializer.BitcoinPacketHeader

--- core/src/main/java/org/bitcoinj/core/BitcoinSerializer.java
+++ core/src/main/java/org/bitcoinj/core/BitcoinSerializer.java
@@ -367,7 +367,7 @@ public class BitcoinSerializer extends MessageSerializer {
             size = (int) readUint32(header, cursor);
             cursor += 4;
 
-            if (size > Message.MAX_SIZE)
+            if (size > Message.MAX_SIZE || size < 0)
                 throw new ProtocolException("Message size too large: " + size);
 
             // Old clients don't send the checksum.
