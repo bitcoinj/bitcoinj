bitcoinj/bitcoinj

commit a53b508049d0febf59dfd3f7e5d75776a0b3f7a8

Fix various bugs and documented unintuitive/suspicious behavior in equals/hashCode/compareTo implementations.

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -444,7 +444,7 @@ public class TransactionOutput extends ChildMessage implements Serializable {
         int result = (int) (value ^ (value >>> 32));
         result = 31 * result + Arrays.hashCode(scriptBytes);
         if (parent != null)
-            result *= parent.getHash().hashCode() + getIndex();
+            result *= parent.getHash().hashCode();
         return result;
     }
 }

core/src/main/java/org/bitcoinj/core/DumpedPrivateKey.java
@@ -90,6 +90,6 @@ public class DumpedPrivateKey extends VersionedChecksummedBytes {
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(bytes, version, compressed);
+        return Objects.hashCode(version, compressed, Arrays.hashCode(bytes));
     }
 }

core/src/main/java/org/bitcoinj/core/GetBlocksMessage.java
@@ -102,14 +102,14 @@ public class GetBlocksMessage extends Message {
         GetBlocksMessage other = (GetBlocksMessage) o;
         return version == other.version &&
                locator.size() == other.locator.size() &&
-               locator.containsAll(other.locator) &&
+               locator.containsAll(other.locator) && // ignores locator ordering
                stopHash.equals(other.stopHash);
     }
 
     @Override
     public int hashCode() {
         int hashCode = (int) version ^ "getblocks".hashCode();
-        for (Sha256Hash aLocator : locator) hashCode ^= aLocator.hashCode();
+        for (Sha256Hash aLocator : locator) hashCode ^= aLocator.hashCode(); // ignores locator ordering
         hashCode ^= stopHash.hashCode();
         return hashCode;
     }

core/src/main/java/org/bitcoinj/core/GetHeadersMessage.java
@@ -49,14 +49,14 @@ public class GetHeadersMessage extends GetBlocksMessage {
         GetHeadersMessage other = (GetHeadersMessage) o;
         return version == other.version &&
                locator.size() == other.locator.size() &&
-               locator.containsAll(other.locator) &&
+               locator.containsAll(other.locator) && // ignores locator ordering
                stopHash.equals(other.stopHash);
     }
 
     @Override
     public int hashCode() {
         int hashCode = (int) version ^ "getheaders".hashCode();
-        for (Sha256Hash aLocator : locator) hashCode ^= aLocator.hashCode();
+        for (Sha256Hash aLocator : locator) hashCode ^= aLocator.hashCode(); // ignores locator ordering
         hashCode ^= stopHash.hashCode();
         return hashCode;
     }

core/src/main/java/org/bitcoinj/core/Sha256Hash.java
@@ -265,6 +265,7 @@ public class Sha256Hash implements Serializable, Comparable<Sha256Hash> {
 
     @Override
     public int compareTo(Sha256Hash o) {
-        return this.hashCode() - o.hashCode();
+        // note that in this implementation compareTo() is not consistent with equals()
+        return this.hashCode() - o.hashCode(); // arbitrary but consistent
     }
 }

core/src/main/java/org/bitcoinj/core/TransactionInput.java
@@ -479,7 +479,6 @@ public class TransactionInput extends ChildMessage implements Serializable {
         if (sequence != input.sequence) return false;
         if (!outpoint.equals(input.outpoint)) return false;
         if (!Arrays.equals(scriptBytes, input.scriptBytes)) return false;
-        if (scriptSig != null ? !scriptSig.equals(input.scriptSig) : input.scriptSig != null) return false;
         if (parent != input.parent) return false;
 
         return true;
@@ -490,7 +489,6 @@ public class TransactionInput extends ChildMessage implements Serializable {
         int result = (int) (sequence ^ (sequence >>> 32));
         result = 31 * result + outpoint.hashCode();
         result = 31 * result + (scriptBytes != null ? Arrays.hashCode(scriptBytes) : 0);
-        result = 31 * result + (scriptSig != null ? scriptSig.hashCode() : 0);
         return result;
     }
 }

core/src/main/java/org/bitcoinj/core/TransactionOutPoint.java
@@ -239,6 +239,6 @@ public class TransactionOutPoint extends ChildMessage implements Serializable {
 
     @Override
     public int hashCode() {
-        return 31 * hash.hashCode() + (int) (index ^ (index >>> 32));
+        return 31 * getHash().hashCode() + (int) (getIndex() ^ (getIndex() >>> 32));
     }
 }

core/src/main/java/org/bitcoinj/core/Utils.java
@@ -578,6 +578,7 @@ public class Utils {
     private static class Pair implements Comparable<Pair> {
         int item, count;
         public Pair(int item, int count) { this.count = count; this.item = item; }
+        // note that in this implementation compareTo() is not consistent with equals()
         @Override public int compareTo(Pair o) { return -Ints.compare(count, o.count); }
     }
 

commit 206f04784f87c3afa5e6897e2ba23af349679f39

Fix wrong balance calculation if identical (apart from the index) outputs are involved.
This regression was triggered by a53b508049d0febf59dfd3f7e5d75776a0b3f7a8, though before that change it only worked by luck.

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -419,8 +419,8 @@ public class TransactionOutput extends ChildMessage {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TransactionOutput other = (TransactionOutput) o;
-        return value == other.value && (parent == null || parent == other.parent)
-            && Arrays.equals(scriptBytes, other.scriptBytes);
+        return value == other.value && (parent == null || (parent == other.parent && getIndex() == other.getIndex()))
+                && Arrays.equals(scriptBytes, other.scriptBytes);
     }
 
     @Override

core/src/test/java/org/bitcoinj/wallet/WalletTest.java
@@ -545,6 +545,16 @@ public class WalletTest extends TestWithWallet {
         assertEquals(v4, wallet.getBalance(Wallet.BalanceType.AVAILABLE));
     }
 
+    @Test
+    public void balanceWithIdenticalOutputs() {
+        assertEquals(Coin.ZERO, wallet.getBalance(BalanceType.ESTIMATED));
+        Transaction tx = new Transaction(PARAMS);
+        tx.addOutput(Coin.COIN, myAddress);
+        tx.addOutput(Coin.COIN, myAddress); // identical to the above
+        wallet.addWalletTransaction(new WalletTransaction(Pool.UNSPENT, tx));
+        assertEquals(Coin.COIN.plus(Coin.COIN), wallet.getBalance(BalanceType.ESTIMATED));
+    }
+
     // Intuitively you'd expect to be able to create a transaction with identical inputs and outputs and get an
     // identical result to Bitcoin Core. However the signatures are not deterministic - signing the same data
     // with the same key twice gives two different outputs. So we cannot prove bit-for-bit compatibility in this test