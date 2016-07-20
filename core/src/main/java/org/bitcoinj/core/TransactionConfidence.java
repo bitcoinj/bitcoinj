a772addc25a3cc748dc4f774f5db86fea2f9446e
Andreas Schildbach <andreas.schildbach@bloq.com>
2016-07-20 16:13:02 +0200

    TransactionConfidence: Print lastBroadcastedAt in toString().

--- core/src/main/java/org/bitcoinj/core/TransactionConfidence.java
+++ core/src/main/java/org/bitcoinj/core/TransactionConfidence.java
@@ -334,7 +334,10 @@ public class TransactionConfidence {
         StringBuilder builder = new StringBuilder();
         int peers = numBroadcastPeers();
         if (peers > 0) {
-            builder.append("Seen by ").append(peers).append(peers > 1 ? " peers. " : " peer. ");
+            builder.append("Seen by ").append(peers).append(peers > 1 ? " peers" : " peer");
+            if (lastBroadcastedAt != null)
+                builder.append(" (most recently: ").append(Utils.dateTimeFormat(lastBroadcastedAt)).append(")");
+            builder.append(". ");
         }
         switch (getConfidenceType()) {
             case UNKNOWN:
