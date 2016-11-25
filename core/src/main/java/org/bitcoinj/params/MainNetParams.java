09b8ae4adf2596e3cc2d46cbf68d34f8b741f0cc
Andreas Schildbach <andreas.schildbach@bloq.com>
2016-11-25 23:29:56 +0100

    Update seeds for mainnet and testnet. Seeds are copied from Bitcoin Core 0.13.1.

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -71,11 +71,13 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         checkpoints.put(200000, Sha256Hash.wrap("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
 
         dnsSeeds = new String[] {
-                "seed.bitcoin.sipa.be",        // Pieter Wuille
-                "dnsseed.bluematt.me",         // Matt Corallo
-                "dnsseed.bitcoin.dashjr.org",  // Luke Dashjr
-                "seed.bitcoinstats.com",       // Chris Decker
-                "seed.bitnodes.io",            // Addy Yeow
+                "seed.bitcoin.sipa.be",         // Pieter Wuille
+                "dnsseed.bluematt.me",          // Matt Corallo
+                "dnsseed.bitcoin.dashjr.org",   // Luke Dashjr
+                "seed.bitcoinstats.com",        // Chris Decker
+                "seed.bitnodes.io",             // Addy Yeow
+                "bitseed.xf2.org",              // Jeff Garzik
+                "seed.bitcoin.jonasschnelli.ch" // Jonas Schnelli
         };
         httpSeeds = new HttpDiscovery.Details[] {
                 // Andreas Schildbach
--- core/src/main/java/org/bitcoinj/params/TestNet3Params.java
+++ core/src/main/java/org/bitcoinj/params/TestNet3Params.java
@@ -58,8 +58,10 @@ public class TestNet3Params extends AbstractBitcoinNetParams {
         alertSigningKey = Utils.HEX.decode("04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
 
         dnsSeeds = new String[] {
-                "testnet-seed.bitcoin.schildbach.de", // Andreas Schildbach
-                "testnet-seed.bitcoin.petertodd.org"  // Peter Todd
+                "testnet-seed.bitcoin.jonasschnelli.ch", // Jonas Schnelli
+                "testnet-seed.bluematt.me",              // Matt Corallo
+                "testnet-seed.bitcoin.petertodd.org",    // Peter Todd
+                "testnet-seed.bitcoin.schildbach.de",    // Andreas Schildbach
         };
         addrSeeds = null;
         bip32HeaderPub = 0x043587CF;
