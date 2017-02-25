af40d03867d7b5ac370f467cd8e0c0eb8291e715
Andreas Schildbach <andreas.schildbach@bloq.com>
2017-02-25 12:37:35 +0100

    BIP32Test: New test vector, see https://github.com/bitcoin/bips/pull/497

--- core/src/test/java/org/bitcoinj/crypto/BIP32Test.java
+++ core/src/test/java/org/bitcoinj/crypto/BIP32Test.java
@@ -114,6 +114,19 @@ public class BIP32Test {
                                     "xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt"
                             )
                     )
+            ),
+            new HDWTestVector(
+                    "4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be",
+                    "xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6",
+                    "xpub661MyMwAqRbcEZVB4dScxMAdx6d4nFc9nvyvH3v4gJL378CSRZiYmhRoP7mBy6gSPSCYk6SzXPTf3ND1cZAceL7SfJ1Z3GC8vBgp2epUt13",
+                    Arrays.asList(
+                            new HDWTestVector.DerivedTestCase(
+                                    "Test3 m/0H",
+                                    new ChildNumber[]{new ChildNumber(0, true)},
+                                    "xprv9uPDJpEQgRQfDcW7BkF7eTya6RPxXeJCqCJGHuCJ4GiRVLzkTXBAJMu2qaMWPrS7AANYqdq6vcBcBUdJCVVFceUvJFjaPdGZ2y9WACViL4L",
+                                    "xpub68NZiKmJWnxxS6aaHmn81bvJeTESw724CRDs6HbuccFQN9Ku14VQrADWgqbhhTHBaohPX4CjNLf9fq9MYo6oDaPPLPxSb7gwQN3ih19Zm4Y"
+                            )
+                   )
             )
     };
 
@@ -125,6 +138,11 @@ public class BIP32Test {
     @Test
     public void testVector2() throws Exception {
         testVector(1);
+    }
+
+    @Test
+    public void testVector3() throws Exception {
+        testVector(2);
     }
 
     private void testVector(int testCase) {
