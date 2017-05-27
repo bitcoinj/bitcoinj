80baadd9abe26d051830d15f1ef82bf545ee4e46
b00lean <b00lean@b00lean.net>
2017-05-27 18:34:58 +0200

    fixed WIF address format

--- core/src/main/java/org/litecoinj/params/MainNetParams.java
+++ core/src/main/java/org/litecoinj/params/MainNetParams.java
@@ -37,8 +37,8 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         interval = INTERVAL;
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1e0fffffL);
-        dumpedPrivateKeyHeader = 128;
         addressHeader = 48;
+        dumpedPrivateKeyHeader = 128 + addressHeader;
         p2shHeader = 5;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 9333;
