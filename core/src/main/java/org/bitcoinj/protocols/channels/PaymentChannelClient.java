d9f0aa8e26f5d2216509f7a8d062eed5190a9b6e
zkejid <zkejid@gmail.com>
2017-07-24 21:24:31 +0700

    Avoid NPE on building message with protobuff
     - 'explanation' argument in builder should not be null.

--- core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelClient.java
+++ core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelClient.java
@@ -475,8 +475,10 @@ public class PaymentChannelClient implements IPaymentChannelClient {
             } catch (VerificationException e) {
                 log.error("Caught verification exception handling message from server", e);
                 errorBuilder = Protos.Error.newBuilder()
-                        .setCode(Protos.Error.ErrorCode.BAD_TRANSACTION)
-                        .setExplanation(e.getMessage());
+                        .setCode(Protos.Error.ErrorCode.BAD_TRANSACTION);
+                if (e.getMessage() != null) {
+                    errorBuilder.setExplanation(e.getMessage());
+                }
                 closeReason = CloseReason.REMOTE_SENT_INVALID_MESSAGE;
             } catch (IllegalStateException e) {
                 log.error("Caught illegal state exception handling message from server", e);
