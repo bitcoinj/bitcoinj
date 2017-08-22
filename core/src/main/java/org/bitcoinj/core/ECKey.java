bitcoinj/bitcoinj

commit e5f14e789a391bb1ed89e78943a25eed12943334

ECDSASignature.decodeFromDER(): Now throws IllegalArgumentException if something with the input bytes is wrong. RuntimeException is bad!

core/src/main/java/org/bitcoinj/core/ECKey.java
@@ -574,13 +574,13 @@ public class ECKey implements EncryptableItem {
             }
         }
 
-        public static ECDSASignature decodeFromDER(byte[] bytes) {
+        public static ECDSASignature decodeFromDER(byte[] bytes) throws IllegalArgumentException {
             ASN1InputStream decoder = null;
             try {
                 decoder = new ASN1InputStream(bytes);
                 DLSequence seq = (DLSequence) decoder.readObject();
                 if (seq == null)
-                    throw new RuntimeException("Reached past end of ASN.1 stream.");
+                    throw new IllegalArgumentException("Reached past end of ASN.1 stream.");
                 ASN1Integer r, s;
                 try {
                     r = (ASN1Integer) seq.getObjectAt(0);
@@ -592,7 +592,7 @@ public class ECKey implements EncryptableItem {
                 // Thus, we always use the positive versions. See: http://r6.ca/blog/20111119T211504Z.html
                 return new ECDSASignature(r.getPositiveValue(), s.getPositiveValue());
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new IllegalArgumentException(e);
             } finally {
                 if (decoder != null)
                     try { decoder.close(); } catch (IOException x) {}

core/src/main/java/org/bitcoinj/wallet/DefaultRiskAnalysis.java
@@ -183,7 +183,7 @@ public class DefaultRiskAnalysis implements RiskAnalysis {
                 ECDSASignature signature;
                 try {
                     signature = ECKey.ECDSASignature.decodeFromDER(chunk.data);
-                } catch (RuntimeException x) {
+                } catch (IllegalArgumentException x) {
                     // Doesn't look like a signature.
                     signature = null;
                 }

commit 3ef5f9f2d7b4ef911af5813c58a43817d3bf399e

ECKey.decodeFromDER(): Also throws IllegalArgumentException if unexpected class was read.
Fixes a regression that was introduced with commit e5f14e789a391bb1ed89e78943a25eed12943334.

core/src/main/java/org/bitcoinj/core/ECKey.java
@@ -578,9 +578,12 @@ public class ECKey implements EncryptableItem {
             ASN1InputStream decoder = null;
             try {
                 decoder = new ASN1InputStream(bytes);
-                DLSequence seq = (DLSequence) decoder.readObject();
-                if (seq == null)
+                final ASN1Primitive seqObj = decoder.readObject();
+                if (seqObj == null)
                     throw new IllegalArgumentException("Reached past end of ASN.1 stream.");
+                if (!(seqObj instanceof DLSequence))
+                    throw new IllegalArgumentException("Read unexpected class: " + seqObj.getClass().getName());
+                final DLSequence seq = (DLSequence) seqObj;
                 ASN1Integer r, s;
                 try {
                     r = (ASN1Integer) seq.getObjectAt(0);