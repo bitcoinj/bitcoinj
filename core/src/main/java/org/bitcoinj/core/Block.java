73fe9e6ab51501d498cf1a970aca208ea049adff
Daniel James <dwhjames@gmail.com>
2017-12-25 18:18:14 -0800

    Block: Avoid NPE in hasTransactions()
    Field is nullable;
    
    ```java
    @Nullable List<Transaction> transactions;
    ```

--- core/src/main/java/org/bitcoinj/core/Block.java
+++ core/src/main/java/org/bitcoinj/core/Block.java
@@ -1002,7 +1002,7 @@ public class Block extends Message {
      * purely a header).
      */
     public boolean hasTransactions() {
-        return !this.transactions.isEmpty();
+        return (this.transactions != null) && !this.transactions.isEmpty();
     }
 
     /**
