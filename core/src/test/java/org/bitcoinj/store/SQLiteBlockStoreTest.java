abfb9b87396eb7f9cd13438ec618630c938006fc
BigAdam2005 <a.topolski.at@gmail.com>
2018-07-05 13:37:54 +0200

    Added unit tests

--- core/src/test/java/org/bitcoinj/store/SQLiteBlockStoreTest.java
+++ core/src/test/java/org/bitcoinj/store/SQLiteBlockStoreTest.java
@@ -1,9 +1,9 @@
 package org.bitcoinj.store;
 
-import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.StoredBlock;
+import org.bitcoinj.core.*;
 import org.bitcoinj.params.UnitTestParams;
 import org.junit.Test;
+import org.spongycastle.util.Store;
 
 import java.io.File;
 
@@ -14,10 +14,19 @@ public class SQLiteBlockStoreTest {
 
     @Test
     public void basics() throws Exception {
+        String filePath = File.createTempFile("asd", "t").getAbsolutePath();
         NetworkParameters params = UnitTestParams.get();
-        SQLiteBlockStore store = new SQLiteBlockStore(params, File.createTempFile("asd", "t").getAbsolutePath());
+        SQLiteBlockStore store = new SQLiteBlockStore(params, filePath);
         StoredBlock genesis = store.getChainHead();
         assertEquals(UNITTEST.getGenesisBlock(), genesis.getHeader());
         assertEquals(0, genesis.getHeight());
+        Address to = LegacyAddress.fromBase58(UNITTEST, "mrj2K6txjo2QBcSmuAzHj4nD1oXSEJE1Qo");
+        StoredBlock bl = genesis.build(genesis.getHeader().createNextBlock(to).cloneAsHeader());
+        store.put(bl);
+        assertEquals(bl , store.get(bl.getHeader().getHash()));
+        store.close();
+        store = new SQLiteBlockStore(params, filePath);
+        assertEquals(bl , store.get(bl.getHeader().getHash()));
+        assertEquals(genesis, store.getChainHead());
     }
 }
