9db984dd1637d3c2f6d664a4daea5e1fb83ab7d7
BigAdam2005 <a.topolski.at@gmail.com>
2018-08-04 20:50:08 +0200

    Added blockstore mapping Signed-off-by: BigAdam2005 <a.topolski.at@gmail.com>

--- core/src/main/java/org/bitcoinj/store/SPVBlockStore.java
+++ core/src/main/java/org/bitcoinj/store/SPVBlockStore.java
@@ -16,21 +16,30 @@
 
 package org.bitcoinj.store;
 
-import org.bitcoinj.core.*;
-import org.bitcoinj.utils.*;
-import org.slf4j.*;
+import org.bitcoinj.core.Block;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.StoredBlock;
+import org.bitcoinj.utils.Threading;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.annotation.*;
-import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.concurrent.locks.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.google.common.base.Preconditions.*;
-
-// TODO: Lose the mmap in this class. There are too many platform bugs that require odd workarounds.
 
 /**
  * An SPVBlockStore holds a limited number of block headers in a memory mapped ring buffer. With such a store, you
@@ -38,51 +47,53 @@ import static com.google.common.base.Preconditions.*;
  * but as they are virtually unheard of this is not a significant risk.
  */
 public class SPVBlockStore implements BlockStore {
-    private static final Logger log = LoggerFactory.getLogger(SPVBlockStore.class);
-
-    /** The default number of headers that will be stored in the ring buffer. */
+    /**
+     * The default number of headers that will be stored in the ring buffer.
+     */
     public static final int DEFAULT_CAPACITY = 5000;
+
     public static final String HEADER_MAGIC = "SPVB";
 
-    protected volatile MappedByteBuffer buffer;
+    protected static final int RECORD_SIZE = 32 /* hash */ + StoredBlock.COMPACT_SERIALIZED_SIZE;
+
+    // File format:
+    //   4 header bytes = "SPVB"
+    //   4 cursor bytes, which indicate the offset from the first kb where the next block header should be written.
+    //   4 height bytes
+    //   32 bytes for the hash of the chain head
+    //
+    // For each header (128 bytes)
+    //   32 bytes hash of the header
+    //   12 bytes of chain work
+    //    4 bytes of height
+    //   80 bytes of block header data
+    protected static final int FILE_PROLOGUE_BYTES = 1024;
+
+    private static final Logger log = LoggerFactory.getLogger(SPVBlockStore.class);
+
     protected final NetworkParameters params;
+
+    protected volatile MappedByteBuffer buffer;
+
+    protected Map<Sha256Hash, ChainMap> chainMaps = new HashMap<>();
 
     protected ReentrantLock lock = Threading.lock("SPVBlockStore");
 
-    // The entire ring-buffer is mmapped and accessing it should be as fast as accessing regular memory once it's
-    // faulted in. Unfortunately, in theory practice and theory are the same. In practice they aren't.
-    //
-    // MMapping a file in Java does not give us a byte[] as you may expect but rather a ByteBuffer, and whilst on
-    // the OpenJDK/Oracle JVM calls into the get() methods are compiled down to inlined native code on Android each
-    // get() call is actually a full-blown JNI method under the hood, meaning it's unbelievably slow. The caches
-    // below let us stay in the JIT-compiled Java world without expensive JNI transitions and make a 10x difference!
-    protected LinkedHashMap<Sha256Hash, StoredBlock> blockCache = new LinkedHashMap<Sha256Hash, StoredBlock>() {
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<Sha256Hash, StoredBlock> entry) {
-            return size() > 2050;  // Slightly more than the difficulty transition period.
-        }
-    };
-    // Use a separate cache to track get() misses. This is to efficiently handle the case of an unconnected block
-    // during chain download. Each new block will do a get() on the unconnected block so if we haven't seen it yet we
-    // must efficiently respond.
-    //
-    // We don't care about the value in this cache. It is always notFoundMarker. Unfortunately LinkedHashSet does not
-    // provide the removeEldestEntry control.
-    private static final Object NOT_FOUND_MARKER = new Object();
-    protected LinkedHashMap<Sha256Hash, Object> notFoundCache = new LinkedHashMap<Sha256Hash, Object>() {
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<Sha256Hash, Object> entry) {
-            return size() > 100;  // This was chosen arbitrarily.
-        }
-    };
     // Used to stop other applications/processes from opening the store.
     protected FileLock fileLock = null;
+
     protected RandomAccessFile randomAccessFile = null;
+
+    protected StoredBlock lastChainHead = null;
+
+    private int height;
+
     private int fileLength;
 
     /**
      * Creates and initializes an SPV block store that can hold {@link #DEFAULT_CAPACITY} block headers. Will create the
      * given file if it's missing. This operation will block on disk.
+     *
      * @param file file to use for the block store
      * @throws BlockStoreException if something goes wrong
      */
@@ -93,9 +104,10 @@ public class SPVBlockStore implements BlockStore {
     /**
      * Creates and initializes an SPV block store that can hold a given amount of blocks. Will create the given file if
      * it's missing. This operation will block on disk.
-     * @param file file to use for the block store
+     *
+     * @param file     file to use for the block store
      * @param capacity custom capacity in number of block headers
-     * @param grow wether or not to migrate an existing block store of different capacity
+     * @param grow     wether or not to migrate an existing block store of different capacity
      * @throws BlockStoreException if something goes wrong
      */
     public SPVBlockStore(NetworkParameters params, File file, int capacity, boolean grow) throws BlockStoreException {
@@ -138,13 +150,23 @@ public class SPVBlockStore implements BlockStore {
             // always be correct. Once we establish the mmap the underlying file and channel can go away. Note that
             // the details of mmapping vary between platforms.
             buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, fileLength);
-
             // Check or initialize the header bytes to ensure we don't try to open some random file.
             if (exists) {
                 byte[] header = new byte[4];
                 buffer.get(header);
                 if (!new String(header, StandardCharsets.US_ASCII).equals(HEADER_MAGIC))
                     throw new BlockStoreException("Header bytes do not equal " + HEADER_MAGIC);
+                buffer.position(8);
+                height = buffer.getInt();
+                buffer.position(FILE_PROLOGUE_BYTES);
+                int last;
+                for (int i = 0; i < height; i++) {
+                    byte[] hash = new byte[32];
+                    last = buffer.position();
+                    buffer.get(hash);
+                    buffer.position(last + 128);
+                    chainMaps.put(Sha256Hash.wrap(hash), new ChainMap(last, (last + 128)));
+                }
             } else {
                 initNewStore(params);
             }
@@ -158,10 +180,19 @@ public class SPVBlockStore implements BlockStore {
         }
     }
 
+    /**
+     * Returns the size in bytes of the file that is used to store the chain with the current parameters.
+     */
+    public static final int getFileSize(int capacity) {
+        return RECORD_SIZE * capacity + FILE_PROLOGUE_BYTES /* extra kilobyte for stuff */;
+    }
+
     private void initNewStore(NetworkParameters params) throws Exception {
         byte[] header;
         header = HEADER_MAGIC.getBytes("US-ASCII");
         buffer.put(header);
+        buffer.position(8);
+        buffer.putInt(0);
         // Insert the genesis block.
         lock.lock();
         try {
@@ -175,89 +206,50 @@ public class SPVBlockStore implements BlockStore {
         setChainHead(storedGenesis);
     }
 
-    /** Returns the size in bytes of the file that is used to store the chain with the current parameters. */
-    public static final int getFileSize(int capacity) {
-        return RECORD_SIZE * capacity + FILE_PROLOGUE_BYTES /* extra kilobyte for stuff */;
-    }
-
     @Override
     public void put(StoredBlock block) throws BlockStoreException {
         final MappedByteBuffer buffer = this.buffer;
         if (buffer == null) throw new BlockStoreException("Store closed");
-
         lock.lock();
         try {
             int cursor = getRingCursor(buffer);
-            if (cursor == fileLength) {
-                // Wrapped around.
-                cursor = FILE_PROLOGUE_BYTES;
-            }
             buffer.position(cursor);
             Sha256Hash hash = block.getHeader().getHash();
-            notFoundCache.remove(hash);
+            chainMaps.put(hash, new ChainMap(cursor, cursor + 128));
             buffer.put(hash.getBytes());
             block.serializeCompact(buffer);
+            height++;
             setRingCursor(buffer, buffer.position());
-            blockCache.put(hash, block);
-        } finally { lock.unlock(); }
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     @Nullable
     public StoredBlock get(Sha256Hash hash) throws BlockStoreException {
+        lock.lock();
         final MappedByteBuffer buffer = this.buffer;
         if (buffer == null) throw new BlockStoreException("Store closed");
-
-        lock.lock();
-        try {
-            StoredBlock cacheHit = blockCache.get(hash);
-            if (cacheHit != null)
-                return cacheHit;
-            if (notFoundCache.get(hash) != null)
-                return null;
-
-            // Starting from the current tip of the ring work backwards until we have either found the block or
-            // wrapped around.
-            int cursor = getRingCursor(buffer);
-            final int startingPoint = cursor;
-            final byte[] targetHashBytes = hash.getBytes();
-            byte[] scratch = new byte[32];
-            do {
-                cursor -= RECORD_SIZE;
-                if (cursor < FILE_PROLOGUE_BYTES) {
-                    // We hit the start, so wrap around.
-                    cursor = fileLength - RECORD_SIZE;
-                }
-                // Cursor is now at the start of the next record to check, so read the hash and compare it.
-                buffer.position(cursor);
-                buffer.get(scratch);
-                if (Arrays.equals(scratch, targetHashBytes)) {
-                    // Found the target.
-                    StoredBlock storedBlock = StoredBlock.deserializeCompact(params, buffer);
-                    blockCache.put(hash, storedBlock);
-                    return storedBlock;
-                }
-            } while (cursor != startingPoint);
-            // Not found.
-            notFoundCache.put(hash, NOT_FOUND_MARKER);
-            return null;
-        } catch (ProtocolException e) {
-            throw new RuntimeException(e);  // Cannot happen.
-        } finally { lock.unlock(); }
+        ChainMap map = chainMaps.get(hash);
+        if (map == null) return null;
+        byte[] hashdat = new byte[32];
+        buffer.position(map.start);
+        buffer.get(hashdat);
+        //buffer.position(map.start);
+        lock.unlock();
+        return StoredBlock.deserializeCompact(params, buffer);
     }
-
-    protected StoredBlock lastChainHead = null;
 
     @Override
     public StoredBlock getChainHead() throws BlockStoreException {
         final MappedByteBuffer buffer = this.buffer;
         if (buffer == null) throw new BlockStoreException("Store closed");
-
         lock.lock();
         try {
             if (lastChainHead == null) {
                 byte[] headHash = new byte[32];
-                buffer.position(8);
+                buffer.position(12);
                 buffer.get(headHash);
                 Sha256Hash hash = Sha256Hash.wrap(headHash);
                 StoredBlock block = get(hash);
@@ -266,7 +258,9 @@ public class SPVBlockStore implements BlockStore {
                 lastChainHead = block;
             }
             return lastChainHead;
-        } finally { lock.unlock(); }
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
@@ -278,18 +272,21 @@ public class SPVBlockStore implements BlockStore {
         try {
             lastChainHead = chainHead;
             byte[] headHash = chainHead.getHeader().getHash().getBytes();
-            buffer.position(8);
+            buffer.position(12);
             buffer.put(headHash);
-        } finally { lock.unlock(); }
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public void close() throws BlockStoreException {
         try {
+            buffer.position(8);
+            buffer.putInt(height);
             buffer.force();
             buffer = null;  // Allow it to be GCd and the underlying file mapping to go away.
             randomAccessFile.close();
-            blockCache.clear();
         } catch (IOException e) {
             throw new BlockStoreException(e);
         }
@@ -300,21 +297,9 @@ public class SPVBlockStore implements BlockStore {
         return params;
     }
 
-    protected static final int RECORD_SIZE = 32 /* hash */ + StoredBlock.COMPACT_SERIALIZED_SIZE;
-
-    // File format:
-    //   4 header bytes = "SPVB"
-    //   4 cursor bytes, which indicate the offset from the first kb where the next block header should be written.
-    //   32 bytes for the hash of the chain head
-    //
-    // For each header (128 bytes)
-    //   32 bytes hash of the header
-    //   12 bytes of chain work
-    //    4 bytes of height
-    //   80 bytes of block header data
-    protected static final int FILE_PROLOGUE_BYTES = 1024;
-
-    /** Returns the offset from the file start where the latest block should be written (end of prev block). */
+    /**
+     * Returns the offset from the file start where the latest block should be written (end of prev block).
+     */
     private int getRingCursor(ByteBuffer buffer) {
         int c = buffer.getInt(4);
         checkState(c >= FILE_PROLOGUE_BYTES, "Integer overflow");
@@ -324,5 +309,25 @@ public class SPVBlockStore implements BlockStore {
     private void setRingCursor(ByteBuffer buffer, int newCursor) {
         checkArgument(newCursor >= 0);
         buffer.putInt(4, newCursor);
+    }
+
+    class ChainMap {
+        private int start;
+
+        private int end;
+
+        ChainMap(int start, int end) {
+
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
     }
 }
