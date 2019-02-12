0b6caf975e7d95ee1da031657a31e797d7a8b81e
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2019-02-12 15:02:20 -0300

    PeerGroup.addAddress(): don't increase max connections if address already exist

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -860,22 +860,27 @@ public class PeerGroup implements TransactionBroadcaster {
         int newMax;
         lock.lock();
         try {
-            addInactive(peerAddress);
-            newMax = getMaxConnections() + 1;
+            if(addInactive(peerAddress)) {
+                newMax = getMaxConnections() + 1;
+                setMaxConnections(newMax);
+            }
         } finally {
             lock.unlock();
         }
-        setMaxConnections(newMax);
     }
 
-    private void addInactive(PeerAddress peerAddress) {
+    // Adds peerAddress to backoffMap map nd inactives queue.
+    // Returns true if it was added, false if it was already there.
+    private boolean addInactive(PeerAddress peerAddress) {
         lock.lock();
         try {
             // Deduplicate
-            if (backoffMap.containsKey(peerAddress))
-                return;
+            if (backoffMap.containsKey(peerAddress)) {
+                return false;
+            }
             backoffMap.put(peerAddress, new ExponentialBackoff(peerBackoffParams));
             inactives.offer(peerAddress);
+            return true;
         } finally {
             lock.unlock();
         }
