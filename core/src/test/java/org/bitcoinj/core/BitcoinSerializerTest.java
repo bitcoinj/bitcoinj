e5242130536ecfcbdecce59f274a0b78b2221652
liudongpo <1196089730@qq.com>
2019-03-11 15:35:42 +0800

    change the assertTrue() to assertArrayEquals()

--- core/src/test/java/org/bitcoinj/core/BitcoinSerializerTest.java
+++ core/src/test/java/org/bitcoinj/core/BitcoinSerializerTest.java
@@ -116,7 +116,7 @@ public class BitcoinSerializerTest {
         assertTrue(transaction.isCached());
         bos = new ByteArrayOutputStream();
         serializer.serialize(transaction, bos);
-        assertTrue(Arrays.equals(TRANSACTION_MESSAGE_BYTES, bos.toByteArray()));
+        assertArrayEquals(TRANSACTION_MESSAGE_BYTES, bos.toByteArray());
 
         // deserialize/reserialize to check for equals.  Set a field to it's existing value to trigger uncache
         transaction = (Transaction) serializer.deserialize(ByteBuffer.wrap(TRANSACTION_MESSAGE_BYTES));
@@ -127,7 +127,7 @@ public class BitcoinSerializerTest {
 
         bos = new ByteArrayOutputStream();
         serializer.serialize(transaction, bos);
-        assertTrue(Arrays.equals(TRANSACTION_MESSAGE_BYTES, bos.toByteArray()));
+        assertArrayEquals(TRANSACTION_MESSAGE_BYTES, bos.toByteArray());
     }
 
     /**
