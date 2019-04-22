94c5c69b41814aa48af5d7ac66bab8706ccc5c2f
Tiberiu Nitan <tiberiu.nitan@tremend.com>
2019-04-22 09:12:35 +0300

    merged try block in writeBinaryCheckpoints

--- tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
+++ tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
@@ -172,27 +172,26 @@ public class BuildCheckpoints {
     }
 
     private static void writeBinaryCheckpoints(TreeMap<Integer, StoredBlock> checkpoints, File file) throws Exception {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(file, false)) {
-            MessageDigest digest = Sha256Hash.newDigest();
-            try (
-                    DigestOutputStream digestOutputStream = new DigestOutputStream(fileOutputStream, digest);
-                    DataOutputStream dataOutputStream = new DataOutputStream(digestOutputStream)
-            ) {
-                digestOutputStream.on(false);
-                dataOutputStream.writeBytes("CHECKPOINTS 1");
-                dataOutputStream.writeInt(0);  // Number of signatures to read. Do this later.
-                digestOutputStream.on(true);
-                dataOutputStream.writeInt(checkpoints.size());
-                ByteBuffer buffer = ByteBuffer.allocate(StoredBlock.COMPACT_SERIALIZED_SIZE);
-                for (StoredBlock block : checkpoints.values()) {
-                    block.serializeCompact(buffer);
-                    dataOutputStream.write(buffer.array());
-                    buffer.position(0);
-                }
-                Sha256Hash checkpointsHash = Sha256Hash.wrap(digest.digest());
-                System.out.println("Hash of checkpoints data is " + checkpointsHash);
-                System.out.println("Checkpoints written to '" + file.getCanonicalPath() + "'.");
+        MessageDigest digest = Sha256Hash.newDigest();
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file, false);
+                DigestOutputStream digestOutputStream = new DigestOutputStream(fileOutputStream, digest);
+                DataOutputStream dataOutputStream = new DataOutputStream(digestOutputStream)
+        ) {
+            digestOutputStream.on(false);
+            dataOutputStream.writeBytes("CHECKPOINTS 1");
+            dataOutputStream.writeInt(0);  // Number of signatures to read. Do this later.
+            digestOutputStream.on(true);
+            dataOutputStream.writeInt(checkpoints.size());
+            ByteBuffer buffer = ByteBuffer.allocate(StoredBlock.COMPACT_SERIALIZED_SIZE);
+            for (StoredBlock block : checkpoints.values()) {
+                block.serializeCompact(buffer);
+                dataOutputStream.write(buffer.array());
+                buffer.position(0);
             }
+            Sha256Hash checkpointsHash = Sha256Hash.wrap(digest.digest());
+            System.out.println("Hash of checkpoints data is " + checkpointsHash);
+            System.out.println("Checkpoints written to '" + file.getCanonicalPath() + "'.");
         }
     }
 
