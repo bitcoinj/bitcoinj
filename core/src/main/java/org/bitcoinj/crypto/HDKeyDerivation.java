2ba92af4c90686e1fc77f6888c941f681e574b8a
Andreas Schildbach <andreas.schildbach@bloq.com>
2019-05-02 18:59:46 +0200

    HDKeyDerivation: Remove bogus precondition from deriveChildKeyBytesFromPublic().

--- core/src/main/java/org/bitcoinj/crypto/HDKeyDerivation.java
+++ core/src/main/java/org/bitcoinj/crypto/HDKeyDerivation.java
@@ -186,7 +186,6 @@ public final class HDKeyDerivation {
     }
 
     public static RawKeyBytes deriveChildKeyBytesFromPublic(DeterministicKey parent, ChildNumber childNumber, PublicDeriveMode mode) throws HDDerivationException {
-        checkArgument(!childNumber.isHardened(), "Can't use private derivation with public keys only.");
         byte[] parentPublicKey = parent.getPubKeyPoint().getEncoded(true);
         checkState(parentPublicKey.length == 33, "Parent pubkey must be 33 bytes, but is " + parentPublicKey.length);
         ByteBuffer data = ByteBuffer.allocate(37);
