bitcoinj/bitcoinj

commit d4df1119de3658c813e482d12d3c5436ce962e63

SegwitAddress: Import Script rather than Script.ScriptType.

core/src/main/java/org/bitcoinj/core/SegwitAddress.java
@@ -25,7 +25,6 @@ import javax.annotation.Nullable;
 
 import org.bitcoinj.params.Networks;
 import org.bitcoinj.script.Script;
-import org.bitcoinj.script.Script.ScriptType;
 
 /**
  * <p>Implementation of native segwit addresses. They are composed of two parts:</p>
@@ -135,14 +134,14 @@ public class SegwitAddress extends Address {
      * @return type of output script
      */
     @Override
-    public ScriptType getOutputScriptType() {
+    public Script.ScriptType getOutputScriptType() {
         int version = getWitnessVersion();
         checkState(version == 0);
         int programLength = getWitnessProgram().length;
         if (programLength == WITNESS_PROGRAM_LENGTH_PKH)
-            return ScriptType.P2WPKH;
+            return Script.ScriptType.P2WPKH;
         if (programLength == WITNESS_PROGRAM_LENGTH_SH)
-            return ScriptType.P2WSH;
+            return Script.ScriptType.P2WSH;
         throw new IllegalStateException("Cannot happen.");
     }
 

commit 10c421ad120e165e90412bc4b73f9db139aff35f

fix broken SegwitAddress javadoc links
These were broken by https://github.com/bitcoinj/bitcoinj/commit/d4df1119de3658c813e482d12d3c5436ce962e63

core/src/main/java/org/bitcoinj/core/SegwitAddress.java
@@ -129,7 +129,7 @@ public class SegwitAddress extends Address {
 
     /**
      * Get the type of output script that will be used for sending to the address. This is either
-     * {@link ScriptType#P2WPKH} or {@link ScriptType#P2WSH}.
+     * {@link Script.ScriptType#P2WPKH} or {@link Script.ScriptType#P2WSH}.
      * 
      * @return type of output script
      */