47a1ee2690221c9039cc7a7990de4c52897fce53
Tim Strasser <tim.strasser@uzh.ch>
2019-05-13 18:30:16 +0200

    Add copyright notice

--- core/src/test/java/org/bitcoinj/utils/VersionTallyTest.java
+++ core/src/test/java/org/bitcoinj/utils/VersionTallyTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015 Ross Nicoll.
+ * Copyright 2019 Tim Strasser
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
