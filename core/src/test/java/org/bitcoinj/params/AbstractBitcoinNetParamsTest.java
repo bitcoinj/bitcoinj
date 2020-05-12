0fe5f997981bc2d365fd142b778a87e2c2d191dc
Andreas Schildbach <andreas@schildbach.de>
2020-05-12 11:48:56 +0200

    AbstractBitcoinNetParamsTest: Add test for isRewardHalvingPoint().

---
+++ core/src/test/java/org/bitcoinj/params/AbstractBitcoinNetParamsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.params;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AbstractBitcoinNetParamsTest {
+    private final AbstractBitcoinNetParams BITCOIN_PARAMS = new AbstractBitcoinNetParams() {
+        @Override
+        public String getPaymentProtocolId() {
+            return null;
+        }
+    };
+
+    @Test
+    public void isRewardHalvingPoint() {
+        assertTrue(BITCOIN_PARAMS.isRewardHalvingPoint(209999));
+
+        assertTrue(BITCOIN_PARAMS.isRewardHalvingPoint(419999));
+
+        assertFalse(BITCOIN_PARAMS.isRewardHalvingPoint(629998));
+        assertTrue(BITCOIN_PARAMS.isRewardHalvingPoint(629999));
+        assertFalse(BITCOIN_PARAMS.isRewardHalvingPoint(630000));
+
+        assertTrue(BITCOIN_PARAMS.isRewardHalvingPoint(839999));
+    }
+}
