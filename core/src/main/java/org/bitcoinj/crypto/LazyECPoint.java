097329991ab113953314884db631f8abaaa0e47e
Andreas Schildbach <andreas.schildbach@bloq.com>
2021-05-14 16:29:49 +0200

    LazyECPoint: JavaDoc for the constructors.
     Conflicts:
    	core/src/main/java/org/bitcoinj/crypto/LazyECPoint.java

--- core/src/main/java/org/bitcoinj/crypto/LazyECPoint.java
+++ core/src/main/java/org/bitcoinj/crypto/LazyECPoint.java
@@ -42,11 +42,23 @@ public class LazyECPoint {
     @Nullable
     private ECPoint point;
 
+    /**
+     * Construct a LazyECPoint from a public key. Due to the delayed decoding of the point the validation of the
+     * public key is delayed too, e.g. until a getter is called.
+     *
+     * @param curve a curve the point is on
+     * @param bits  public key bytes
+     */
     public LazyECPoint(ECCurve curve, byte[] bits) {
         this.curve = curve;
         this.bits = bits;
     }
 
+    /**
+     * Construct a LazyECPoint from an already decoded point.
+     *
+     * @param point      the wrapped point
+     */
     public LazyECPoint(ECPoint point) {
         this.point = checkNotNull(point);
         this.curve = null;
