b73372b7023e536ba80b1b1b6037f95bf0f4e079
Sean Gilligan <sean@msgilligan.com>
2021-09-02 15:43:45 -0700

    Further cleanup createGenesisTransaction()
    and add a test of createGenesisBlock()

--- core/src/test/java/org/bitcoinj/core/BlockTest.java
+++ core/src/test/java/org/bitcoinj/core/BlockTest.java
@@ -355,4 +355,13 @@ public class BlockTest {
             //Expected, do nothing
         }
     }
+
+    @Test
+    public void testGenesisBlock() {
+        Block genesisBlock = Block.createGenesis(MainNetParams.get());
+        genesisBlock.setDifficultyTarget(0x1d00ffffL);
+        genesisBlock.setTime(1231006505L);
+        genesisBlock.setNonce(2083236893);
+        assertEquals(Sha256Hash.wrap("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"), genesisBlock.getHash());
+    }
 }
