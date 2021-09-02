56a1e24ed2009dde793c60fe39aed70cea0f21bd
Sean Gilligan <sean@msgilligan.com>
2021-09-02 13:56:00 -0700

    Block: add createGenesisTransaction() method
    * Refactor code from createGenesis()
    * Also replace (unnecessarily broad) catch of Exception with
      catch of IOException

--- core/src/main/java/org/bitcoinj/core/Block.java
+++ core/src/main/java/org/bitcoinj/core/Block.java
@@ -264,25 +264,35 @@ public class Block extends Message {
 
     static Block createGenesis(NetworkParameters n) {
         Block genesisBlock = new Block(n, BLOCK_VERSION_GENESIS);
-        Transaction t = new Transaction(n);
-        try {
-            // A script containing the difficulty bits and the following message:
-            //
-            //   "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
-            byte[] bytes = Utils.HEX.decode
-                    ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
-            t.addInput(new TransactionInput(n, t, bytes));
-            ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
-            Script.writeBytes(scriptPubKeyBytes, Utils.HEX.decode
-                    ("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f"));
-            scriptPubKeyBytes.write(ScriptOpCodes.OP_CHECKSIG);
-            t.addOutput(new TransactionOutput(n, t, FIFTY_COINS, scriptPubKeyBytes.toByteArray()));
-        } catch (Exception e) {
-            // Cannot happen.
-            throw new RuntimeException(e);
-        }
+        Transaction t = createGenesisTransaction(n, genesisTxInputScriptBytes, FIFTY_COINS, genesisTxScriptPubKeyBytes);
         genesisBlock.addTransaction(t);
         return genesisBlock;
+    }
+
+    private static Transaction createGenesisTransaction(NetworkParameters n, byte[] inputScriptBytes, Coin amount, byte[] scriptPubKeyBytes) {
+        Transaction t = new Transaction(n);
+        t.addInput(new TransactionInput(n, t, inputScriptBytes));
+        t.addOutput(new TransactionOutput(n, t, amount, scriptPubKeyBytes));
+        return t;
+    }
+
+    // A script containing the difficulty bits and the following message:
+    //
+    //   "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
+    private static final byte[] genesisTxInputScriptBytes = Utils.HEX.decode
+                ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+
+    private static final byte[] genesisTxScriptPubKeyBytes;
+    static {
+        ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
+        try {
+            Script.writeBytes(scriptPubKeyBytes, Utils.HEX.decode
+                    ("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f"));
+        } catch (IOException e) {
+            throw new RuntimeException(e); // Cannot happen.
+        }
+        scriptPubKeyBytes.write(ScriptOpCodes.OP_CHECKSIG);
+        genesisTxScriptPubKeyBytes = scriptPubKeyBytes.toByteArray();
     }
 
     public int getOptimalEncodingMessageSize() {
