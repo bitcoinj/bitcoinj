33c7521eb8077f664c2068e65e59a732e740f08e
Sean Gilligan <sean@msgilligan.com>
2021-09-06 15:59:45 -0700

    Set UnitTest maxTarget to EASIEST_DIFFICULTY_TARGET
    The tests are currently using a custom value for maxTarget 
    that can’t be generated via CompactBits format.
    
    This PR, changes the string constant to match what would be
    generated by using Utils.decodeCompactBits(EASIEST_DIFFICULTY_TARGET)

--- core/src/main/java/org/bitcoinj/params/UnitTestParams.java
+++ core/src/main/java/org/bitcoinj/params/UnitTestParams.java
@@ -36,7 +36,7 @@ public class UnitTestParams extends AbstractBitcoinNetParams {
         id = ID_UNITTESTNET;
 
         targetTimespan = 200000000;  // 6 years. Just a very big number.
-        maxTarget = new BigInteger("00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16);
+        maxTarget = new BigInteger("007fffff0000000000000000000000000000000000000000000000000000000000", 16);  // equivalent to EASIEST_DIFFICULTY_TARGET
         interval = 10;
         subsidyDecreaseBlockCount = 100;
 
