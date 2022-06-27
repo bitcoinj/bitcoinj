a4299dfb399f34273008243b8a3f8a8a2ab7bcfe
Sean Gilligan <sean@msgilligan.com>
2022-06-27 11:01:53 -0700

    Network: add interface and implement in BitcoinNetwork enum

---
+++ core/src/main/java/org/bitcoinj/base/Network.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.base;
+
+/**
+ * Interface for a generic Bitcoin-like cryptocurrency network. See {@link BitcoinNetwork} for the Bitcoin implementation.
+ */
+public interface Network {
+    /**
+     * The dot-seperated string id for this network. For example {@code "org.bitcoin.production"}
+     * @return String ID for network
+     */
+    String id();
+}
