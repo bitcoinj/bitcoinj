5259cc1d72ca5de0a9a296510836aa1ced9a5581
Andreas Schildbach <andreas.schildbach@bloq.com>
2022-07-16 21:44:04 +0200

    ForwardingService: replace copyright with generic statement

--- examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
+++ examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2013 Google Inc.
- * Copyright 2014 Andreas Schildbach
+ * Copyright by the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
