fc74b524c57e51928e5789491b611c539569289c
Sean Gilligan <sean@msgilligan.com>
2022-07-28 19:24:02 -0700

    Add ForwardingServiceTest to integration-test
    Because the tests use TESTNET, the class is @Disabled.

---
+++ integration-test/src/test/java/org/bitcoinj/examples/ForwardingServiceTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.examples;
+
+import org.bitcoinj.base.BitcoinNetwork;
+import org.bitcoinj.base.ScriptType;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Context;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+
+/**
+ * Forwarding Service Functional/Integration test. Uses {@link BitcoinNetwork#TESTNET} so is {@code @Disabled}.
+ * To run this test comment-out the {@code @Disabled} annotation.
+ */
+@Disabled
+public class ForwardingServiceTest {
+    static final BitcoinNetwork network = BitcoinNetwork.TESTNET;
+    static final Address forwardingAddress = Address.fromKey(NetworkParameters.of(network), new ECKey(), ScriptType.P2WPKH);
+
+    @BeforeEach
+    void setupTest() {
+        Context.propagate(new Context());
+    }
+
+    // Start the service via the static forward() method and immediately interrupt
+    @Test
+    public void startViaStaticForwardAndImmediatelyInterrupt(@TempDir File tempDir) {
+        Thread thread = new Thread(
+                () -> ForwardingService.forward(tempDir, network, forwardingAddress)
+        );
+        thread.start();
+        thread.interrupt();
+    }
+
+    // Instantiate the service, start it, and immediately close it
+    // Because ForwardingService disables "blocking mode" in WalletAppKit, start() returns as soon
+    // the PeerGroup was asynchronously started and the WalletAppKit enters the (Guava) RUNNING state.
+    @Test
+    public void startAndImmediatelyClose(@TempDir File tempDir) {
+        ForwardingService service = new ForwardingService(tempDir, forwardingAddress, network);
+        service.start();
+        service.close();
+    }
+}
