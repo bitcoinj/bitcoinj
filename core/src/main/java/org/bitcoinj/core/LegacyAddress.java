339236a479f74b48ae4e44fc8385a8e2895f2702
Sean Gilligan <sean@msgilligan.com>
2022-08-17 13:10:03 -0700

    Legacy/SegwitAddress: normalize network type
    normalize/canonicalize network member of Address types
     
    LegacyAddress: map SIGNET, REGTEST to TESTNET
    SegwitAddress: map SIGNET to TESTNET
    
    Add tests to verify correct behavoir.

--- core/src/main/java/org/bitcoinj/core/LegacyAddress.java
+++ core/src/main/java/org/bitcoinj/core/LegacyAddress.java
@@ -20,6 +20,7 @@ package org.bitcoinj.core;
 
 import com.google.common.primitives.UnsignedBytes;
 import org.bitcoinj.base.Base58;
+import org.bitcoinj.base.BitcoinNetwork;
 import org.bitcoinj.base.Network;
 import org.bitcoinj.base.exceptions.AddressFormatException;
 import org.bitcoinj.params.Networks;
@@ -63,11 +64,22 @@ public class LegacyAddress extends Address {
      *            20-byte hash of pubkey or script
      */
     private LegacyAddress(Network network, boolean p2sh, byte[] hash160) throws AddressFormatException {
-        super(network, hash160);
+        super(normalizeNetwork(network), hash160);
         if (hash160.length != 20)
             throw new AddressFormatException.InvalidDataLength(
                     "Legacy addresses are 20 byte (160 bit) hashes, but got: " + hash160.length);
         this.p2sh = p2sh;
+    }
+
+    private static Network normalizeNetwork(Network network) {
+        // LegacyAddress does not distinguish between the different testnet types, normalize to TESTNET
+        if (network instanceof BitcoinNetwork) {
+            BitcoinNetwork bitcoinNetwork = (BitcoinNetwork) network;
+            if (bitcoinNetwork == BitcoinNetwork.SIGNET || bitcoinNetwork == BitcoinNetwork.REGTEST) {
+                return BitcoinNetwork.TESTNET;
+            }
+        }
+        return network;
     }
 
     /**
