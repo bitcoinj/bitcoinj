a117a2ef3625a22b6102e5bd21f8eea44760287e
Andreas Schildbach <andreas@schildbach.de>
2023-04-04 23:27:08 +0200

    TransactionWitness: add copyright line

--- core/src/test/java/org/bitcoinj/core/TransactionWitnessTest.java
+++ core/src/test/java/org/bitcoinj/core/TransactionWitnessTest.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright by the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
