828cf58ab12ed8560cd8d531f7512b0db4260ea1
Sean Gilligan <sean@msgilligan.com>
2023-06-09 11:55:13 +0200

    package-info.java: update package JavaDoc

--- core/src/main/java/org/bitcoinj/base/package-info.java
+++ core/src/main/java/org/bitcoinj/base/package-info.java
@@ -15,22 +15,21 @@
  */
 
 /**
- * The {@code base} package provides fundamental types for <b>bitcoinj</b>. These types must have
+ * The {@code base} package provides foundational types for <b>bitcoinj</b>. These types must have
  * minimal dependencies. The criteria for allowed dependencies for {@code base} types are:
  * <ul>
  *     <li>No dependencies on other packages of bitcoinj</li>
- *     <li>No API dependencies on external libraries other than the core JDK</li>
- *     <li>Implementation dependencies on Guava are allowed for now (but should be avoided as much as possible)</li>
+ *     <li>No API dependencies on external libraries other than the core JDK and {@code slf4j-api}</li>
  * </ul>
  * <p>
  * <b>Temporary exception:</b> In the 0.17 release, we are allowing some dependencies on other packages, e.g. to
- * {@link org.bitcoinj.core.NetworkParameters} provided that those references are in <b>deprecated</b> methods. This will
- * smooth migration by allowing users to, for example, replace {@code import org.bitcoinj.base.Address} with
- * {@code import org.bitcoinj.core.Address} as first step of conversion and remove usages of the deprecated methods in
- * a second step.
+ * {@link org.bitcoinj.core.NetworkParameters} or to Guava <i>provided</i> that those references are in <b>deprecated</b> methods.
+ * This smooths migration by allowing users to, for example, replace {@code import org.bitcoinj.core.Address} with
+ * {@code import org.bitcoinj.base.Address} as first step of conversion and then remove usages of the deprecated methods
+ * of {@code Address} in a second step.
  * <p>
- * The base package will help us make bitcoinj more modular as we will use it to break circular dependencies
- * between existing packages. We are also considering splitting {@code base} into a separate JAR/module in a
- * future release.
+ * The base package makes bitcoinj more modular as it breaks circular dependencies between existing packages and provides
+ * a "zero-dependency" foundation for the other packages. In a future release {@code base} will be
+ * split into a separate JAR/module (tentatively {@code bitcoinj-base}.)
  */
 package org.bitcoinj.base;
