29138c2ba0076064d41fcfecd7fad81bfb155af7
Sean Gilligan <sean@msgilligan.com>
2023-08-09 12:31:20 -0700

    TransactionOutput: use messageSize() not serialize().length

--- core/src/main/java/org/bitcoinj/core/TransactionOutput.java
+++ core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -240,7 +240,7 @@ public class TransactionOutput {
         // so dust is a spendable txout less than
         // 98*dustRelayFee/1000 (in satoshis).
         // 294 satoshis at the default rate of 3000 sat/kB.
-        long size = this.serialize().length;
+        long size = this.messageSize();
         final Script script = getScriptPubKey();
         if (ScriptPattern.isP2PKH(script) || ScriptPattern.isP2PK(script) || ScriptPattern.isP2SH(script) || ScriptPattern.isSentToMultisig(script))
             size += 32 + 4 + 1 + 107 + 4; // 148
