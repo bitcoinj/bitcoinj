432fa51168d90650965d1fe3658f05d2ce61491c
Andreas Schildbach <andreas@schildbach.de>
2023-10-09 10:11:33 +0200

    EncryptedData, KeyCrypter, KeyCrypterScrypt: update links to MIT license

--- core/src/main/java/org/bitcoinj/crypto/KeyCrypter.java
+++ core/src/main/java/org/bitcoinj/crypto/KeyCrypter.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://opensource.org/licenses/mit-license.php
+ *    https://opensource.org/license/mit/
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
