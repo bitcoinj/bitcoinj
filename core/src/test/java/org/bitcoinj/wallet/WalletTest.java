cef8b77e477d66908984492a7291f2d0d7a89d76
Andreas Schildbach <andreas@schildbach.de>
2024-09-02 16:18:44 +0200

    Wallet: remove references to deprecated autosaveToFile()

--- core/src/test/java/org/bitcoinj/wallet/WalletTest.java
+++ core/src/test/java/org/bitcoinj/wallet/WalletTest.java
@@ -101,7 +101,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -1708,7 +1707,7 @@ public class WalletTest extends TestWithWallet {
         File f = File.createTempFile("bitcoinj-unit-test", null);
         Sha256Hash hash1 = Sha256Hash.of(f);
         // Start with zero delay and ensure the wallet file changes after adding a key.
-        wallet.autosaveToFile(f, 0, TimeUnit.SECONDS, null);
+        wallet.autosaveToFile(f, Duration.ZERO, null);
         ECKey key = wallet.freshReceiveKey();
         Sha256Hash hash2 = Sha256Hash.of(f);
         assertFalse("Wallet not saved after generating fresh key", hash1.equals(hash2));  // File has changed.
@@ -1729,7 +1728,7 @@ public class WalletTest extends TestWithWallet {
         final CountDownLatch latch = new CountDownLatch(3);
         File f = File.createTempFile("bitcoinj-unit-test", null);
         Sha256Hash hash1 = Sha256Hash.of(f);
-        wallet.autosaveToFile(f, 1, TimeUnit.SECONDS,
+        wallet.autosaveToFile(f, Duration.ofSeconds(1),
                 new WalletFiles.Listener() {
                     @Override
                     public void onBeforeAutoSave(File tempFile) {
