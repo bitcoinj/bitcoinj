328da890c4cdd6d92685223428ec930576251369
Andreas Schildbach <andreas@schildbach.de>
2025-01-27 14:49:29 +0100

    Tag 0.16.5

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -44,7 +44,7 @@ import java.util.Objects;
 public class VersionMessage extends Message {
 
     /** The version of this library release, as a string. */
-    public static final String BITCOINJ_VERSION = "0.16.5-SNAPSHOT";
+    public static final String BITCOINJ_VERSION = "0.16.5";
     /** The value that is prepended to the subVer field of this application. */
     public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
 
