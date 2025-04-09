0efd5329f5ba92e64944aabc09b8eeb5c68c3f32
Sean Gilligan <sean@msgilligan.com>
2025-04-08 19:04:10 -0700

    HDPath: (internal) don't assume HDPath implements List
    Fix two internal calls to pass List<ChildNumber> rather
    than assume HDPath implements List<>. This is in preparation for
    HDPath no longer implementing List<>.

--- core/src/main/java/org/bitcoinj/crypto/HDPath.java
+++ core/src/main/java/org/bitcoinj/crypto/HDPath.java
@@ -147,7 +147,7 @@ public abstract class HDPath extends AbstractList<ChildNumber> {
 
         @Override
         public HDFullPath extend(HDPath.HDPartialPath partialPath) {
-            return new HDFullPath(this.hasPrivateKey, extendInternal(partialPath));
+            return new HDFullPath(this.hasPrivateKey, extendInternal(partialPath.childNumbers));
         }
 
         @Override
@@ -184,7 +184,7 @@ public abstract class HDPath extends AbstractList<ChildNumber> {
 
         @Override
         public HDPartialPath extend(HDPath.HDPartialPath partialPath) {
-            return new HDPartialPath(extendInternal(partialPath));
+            return new HDPartialPath(extendInternal(partialPath.childNumbers));
         }
 
         @Override
