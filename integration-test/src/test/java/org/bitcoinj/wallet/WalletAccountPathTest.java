0b48fb8cb10b46ad01dced9d41d8ec440eb3bcae
Sean Gilligan <sean@msgilligan.com>
2025-04-26 10:09:17 -0700

    WalletAccountPathTest: use static constructor `DeterministicSeed.ofMnemonic()`
    Previously we were calling a package-private constructor destined
    to become private in future.

--- integration-test/src/test/java/org/bitcoinj/wallet/WalletAccountPathTest.java
+++ integration-test/src/test/java/org/bitcoinj/wallet/WalletAccountPathTest.java
@@ -80,7 +80,7 @@ public class WalletAccountPathTest {
     // Create a wallet, save it to a file, then reload from a file
     private static Wallet createWallet(File walletFile, Network network, KeyChainGroupStructure structure, ScriptType outputScriptType) throws IOException, UnreadableWalletException {
         Context.propagate(new Context());
-        DeterministicSeed seed = new DeterministicSeed(testWalletMnemonic, null, "", Instant.now());
+        DeterministicSeed seed = DeterministicSeed.ofMnemonic(testWalletMnemonic, "", Instant.now());
         Wallet wallet = Wallet.fromSeed(network, seed, outputScriptType, structure);
         wallet.saveToFile(walletFile);
         return Wallet.loadFromFile(walletFile);
