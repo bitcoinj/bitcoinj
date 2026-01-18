dc40791697e9ee01024633a37c5af1c39cc4b18d
Sean Gilligan <sean@msgilligan.com>
2026-01-18 11:33:37 -0800

    PeerAddress: update Javadoc now that we are immutable and thread-safe

--- core/src/main/java/org/bitcoinj/core/PeerAddress.java
+++ core/src/main/java/org/bitcoinj/core/PeerAddress.java
@@ -46,7 +46,7 @@ import static org.bitcoinj.base.internal.Preconditions.checkArgument;
  * A PeerAddress holds an IP address and port number representing the network location of
  * a peer in the Bitcoin P2P network. It exists primarily for serialization purposes.
  * <p>
- * Instances of this class are not safe for use by multiple threads.
+ * This class is immutable and thread-safe.
  */
 public class PeerAddress {
     @Nullable
