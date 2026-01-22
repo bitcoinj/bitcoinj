8276dc31bf419b5c8a4b3a69af8e19d86d5c10a4
Sean Gilligan <sean@msgilligan.com>
2026-01-22 08:37:45 -0800

    package-info.java: add package-info for bitcoinj-test-support

---
+++ test-support/src/main/java/org/bitcoinj/test/support/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Test support classes that depend upon {@code bitcoinj-base} and can be used in both
+ * {@code bitcoinj-base} and {@code bitcoinj-core} tests.
+ */
+@NullMarked
+package org.bitcoinj.test.support;
+
+import org.jspecify.annotations.NullMarked;
