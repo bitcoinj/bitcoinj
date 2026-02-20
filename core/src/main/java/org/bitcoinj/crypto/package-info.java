a6bc2589b01bdc1c8b33e98fa5708f8adff62fc8
Sean Gilligan <sean@msgilligan.com>
2026-02-20 09:12:22 -0800

    package-info.java: remove mentions of removed functionality from package `o.b.crypto`
    Both the payment protocol and the DRMWorkaround have been removed a while ago.

--- core/src/main/java/org/bitcoinj/crypto/package-info.java
+++ core/src/main/java/org/bitcoinj/crypto/package-info.java
@@ -16,10 +16,7 @@
 
 /**
  * The crypto package contains classes that work with key derivation algorithms like scrypt (passwords to AES keys),
- * BIP 32 hierarchies (chains of keys from a root seed), X.509 utilities for the payment protocol and other general
- * cryptography tasks. It also contains a class that can disable the (long since obsolete) DRM Java/US Govt imposes
- * on strong crypto. This is legal because Oracle got permission to ship strong AES to everyone years ago but hasn't
- * bothered to actually remove the logic barriers.
+ * BIP 32 hierarchies (chains of keys from a root seed) and other general cryptography tasks.
  */
 @NullMarked
 package org.bitcoinj.crypto;
