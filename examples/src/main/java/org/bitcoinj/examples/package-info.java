21db52adbce53349d3d820ef81b1afcbde21aaba
Sean Gilligan <sean@msgilligan.com>
2026-03-16 15:37:52 -0700

    package-info.java: add missing package-info to package `o.b.examples`

---
+++ examples/src/main/java/org/bitcoinj/examples/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * <b>bitcoinj<b> example applications including {@link org.bitcoinj.examples.ForwardingService} which
+ * is featured in the <b>Getting Started</b> tutorial.
+ * @see <a href="https://bitcoinj.org/getting-started-java">Getting Started</a>
+ */
+package org.bitcoinj.examples;
