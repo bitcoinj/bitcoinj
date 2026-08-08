5049bf6283f6b63d49711cd2a568c8646aa46289
Sean Gilligan <sean@msgilligan.com>
2026-08-07 17:00:26 -0700

    TransactionBag: move getConnectedOutput() from TransactionInput
    This method can be static and is for internal use only.

--- core/src/main/java/org/bitcoinj/core/TransactionInput.java
+++ core/src/main/java/org/bitcoinj/core/TransactionInput.java
@@ -393,12 +393,10 @@ public class TransactionInput {
      *
      * @return The TransactionOutput or null if the transactions map doesn't contain the referenced tx.
      */
+    @Deprecated
     @Nullable
     TransactionOutput getConnectedOutput(Map<Sha256Hash, Transaction> transactions) {
-        Transaction tx = transactions.get(outpoint.hash());
-        return (tx != null)
-                ? tx.getOutput(outpoint)
-                : null;
+        return TransactionBag.getConnectedOutput(transactions, outpoint);
     }
 
     /**
