59e52e2aac8e6c43ca742ada8a4efdea53ab7c71
Sean Gilligan <sean@msgilligan.com>
2026-08-11 15:13:49 -0700

    Secp256k1Constants: add constants for algorithm and format strings

--- base/src/main/java/org/bitcoinj/base/internal/Secp256k1Constants.java
+++ base/src/main/java/org/bitcoinj/base/internal/Secp256k1Constants.java
@@ -26,6 +26,10 @@ import java.security.spec.EllipticCurve;
  * It will eventually be replaces by similar definitions in <b>secp256k1-jdk</b>
  */
 public interface Secp256k1Constants {
+    String ALGORITHM_NAME = "secp256k1";
+    String COMPRESSED_FORMAT_NAME = "Compressed SEC";
+    String UNCOMPRESSED_FORMAT_NAME = "Uncompressed SEC";
+
     /**
      * The prime {@code P}, that defines the secp256k1 field.
      * <p>
