6081a50f706cabbc7ee5ba0b7ac2f965fbc68607
Sean Gilligan <sean@msgilligan.com>
2026-08-20 08:43:27 -0700

    ECKey: remove unused protected constructor

--- core/src/main/java/org/bitcoinj/crypto/ECKey.java
+++ core/src/main/java/org/bitcoinj/crypto/ECKey.java
@@ -237,16 +237,6 @@ public class ECKey implements EncryptableItem, ECPublicKey {
     }
 
     /**
-     * Construct a compressed ECKey. This constructor can be used for P2PWKH and HD keys that
-     * always have addresses generated using compressed serialization.
-     * @param priv optional private key
-     * @param pub a Bouncy Castle point
-     */
-    protected ECKey(@Nullable BigInteger priv, ECPoint pub) {
-        this(priv, Objects.requireNonNull(pub), true);
-    }
-
-    /**
      * ECKey canonical constructor.
      * @param priv optional private key
      * @param pub a Bouncy Castle point
